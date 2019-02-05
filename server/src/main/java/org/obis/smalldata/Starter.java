11206d52eaa66342b2a768907df6c9ed89b6062a
kurt.sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-02-05 14:51:23 +0100

    jlink resources issue, use shadowjar instead

--- server/src/main/java/org/obis/smalldata/Starter.java
+++ server/src/main/java/org/obis/smalldata/Starter.java
@@ -15,18 +15,14 @@ public class Starter extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
-    System.out.println("config() -> " + config().getInteger("http.port", 8080));
+    System.out.println("config() -> " + config().getInteger("http.port", 8000));
     System.out.println("getenv() -> " + System.getenv("HTTP_PORT"));
-    int port = config().getInteger("http.port", 8080);
+    int port = config().getInteger("http.port", 8008);
 
-    HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
-    router.route().handler(StaticHandler.create());
-    server.requestHandler(req ->
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!")
-    );
+    router.get("/smalldata/*").handler(StaticHandler.create());
+
+    HttpServer server = vertx.createHttpServer().requestHandler(router);
     server.listen(port, http -> {
       if (http.succeeded()) {
         startFuture.complete();
@@ -36,20 +32,4 @@ public class Starter extends AbstractVerticle {
       }
     });
   }
-
-  public static void main (String... args) {
-    Vertx vertx = Vertx.vertx();
-    ConfigRetriever retriever = ConfigRetriever.create(vertx);
-
-    retriever.getConfig(json -> {
-      JsonObject result = json.result();
-      vertx.close();
-
-      VertxOptions options = new VertxOptions(result);
-      Vertx newVertx = Vertx.vertx(options);
-
-      newVertx.deployVerticle(Starter.class.getName(), new DeploymentOptions().setConfig(result));
-    });
-  }
-
 }
