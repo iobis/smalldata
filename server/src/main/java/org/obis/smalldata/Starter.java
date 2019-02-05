2ec3319e768a2f742a5ffc8bd3c7d294472731d6
kurt.sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-02-05 15:02:02 +0100

    remove unnecessary imports

--- server/src/main/java/org/obis/smalldata/Starter.java
+++ server/src/main/java/org/obis/smalldata/Starter.java
@@ -1,13 +1,8 @@
 package org.obis.smalldata;
 
-import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
-import io.vertx.core.DeploymentOptions;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 
