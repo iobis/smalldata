5660fc757697b5adec41a2d7721a03b4537539c2
Oleg Tsvetkov <olecve@gmail.com>
2019-02-05 16:25:33 +0200

    Make it pass

--- dwca-lib/src/Button/Button.js
+++ dwca-lib/src/Button/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Button() {
-  return <button onClick={() => console.log('hello')}>click me!</button>
+  return <button onClick={() => console.log('hello')}>click me</button>
 }
