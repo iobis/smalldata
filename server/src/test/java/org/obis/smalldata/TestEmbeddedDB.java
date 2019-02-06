600489eb9ae523e96c724ca1104cfef302e46e20
Oleg Tsvetkov <olecve@gmail.com>
2019-02-06 18:59:30 +0200

    Increate timeout in mongodb test and improve code formatting

--- server/src/test/java/org/obis/smalldata/TestEmbeddedDB.java
+++ server/src/test/java/org/obis/smalldata/TestEmbeddedDB.java
@@ -28,54 +28,55 @@ public class TestEmbeddedDB {
   private static final MongodStarter starter = MongodStarter.getDefaultInstance();
 
   private MongodExecutable mongodExe;
-   private MongodProcess mongod;
+  private MongodProcess mongod;
   private MongoClient client;
 
   @BeforeEach
-  public void beforeEach(Vertx vertx, VertxTestContext testContext) throws Exception {
-
-
-    vertx.deployVerticle(new AbstractVerticle() {
-                           @Override
-                           public void start() throws Exception {
-                             Logger.info("started dummy");
-                             String bindIp = "localhost";
-                             int port = 12345;
-                             mongodExe = starter.prepare(new MongodConfigBuilder()
-                               .version(Version.Main.PRODUCTION)
-                               .net(new Net(bindIp, port, Network.localhostIsIPv6()))
-                               .build());
-                             mongod = mongodExe.start();
-                             client = MongoClient.createNonShared(vertx,
-                               new JsonObject()
-                                 .put("host", bindIp)
-                                 .put("port", port));
-                             Logger.info("Running client {}", client);
-                           }},
+  public void beforeEach(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(
+      new AbstractVerticle() {
+        @Override
+        public void start() throws Exception {
+          Logger.info("started dummy");
+          String bindIp = "localhost";
+          int port = 12345;
+          mongodExe = starter.prepare(new MongodConfigBuilder()
+            .version(Version.Main.PRODUCTION)
+            .net(new Net(bindIp, port, Network.localhostIsIPv6()))
+            .build());
+          mongod = mongodExe.start();
+          client = MongoClient.createNonShared(vertx,
+            new JsonObject()
+              .put("host", bindIp)
+              .put("port", port));
+          Logger.info("Running client {}", client);
+        }
+      },
       testContext.succeeding(id -> {
         Logger.info("succeeding {}", id);
         testContext.completeNow();
       }));
   }
 
-  @Test
-  @DisplayName("Should create a new collection")
-  @Timeout(value = 3, timeUnit = TimeUnit.SECONDS)
-  public void shouldCreateNewObjectInEmbeddedMongoDb(Vertx vertx, VertxTestContext testContext) {
-    Logger.info("-- Running test");
-    client.createCollection("testcollection",
-      res -> {
-        Logger.info("result: {}", res);
-        testContext.completeNow();
-      });
-  }
-
   @AfterEach
-  public void afterEach() throws Exception {
+  public void afterEach() {
     Logger.info("afterEach...");
     if (this.mongod != null) {
       this.mongod.stop();
       this.mongodExe.stop();
     }
+  }
+
+  @Test
+  @DisplayName("Should create a new collection")
+  @Timeout(value = 5, timeUnit = TimeUnit.SECONDS)
+  public void shouldCreateNewObjectInEmbeddedMongoDb(Vertx vertx, VertxTestContext testContext) {
+    Logger.info("-- Running test");
+    client.createCollection(
+      "testcollection",
+      res -> {
+        Logger.info("result: {}", res);
+        testContext.completeNow();
+      });
   }
 }
