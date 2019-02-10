342702c6b5ed0bfc9c2ecfedc2940f33a5e955f1
Oleg Tsvetkov <olecve@gmail.com>
2019-02-10 22:17:52 +0200

    Fix teard down of EmbeddedDbWithPathTest by releaseing resources properly
    also improve time to run by executing population and deployment in beforeAll method

--- server/src/test/java/org/obis/smalldata/db/EmbeddedDbWithPathTest.java
+++ server/src/test/java/org/obis/smalldata/db/EmbeddedDbWithPathTest.java
@@ -9,7 +9,10 @@ import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.pmw.tinylog.Logger;
 
@@ -29,20 +32,16 @@ public class EmbeddedDbWithPathTest {
   private static File dbPath;
 
   @BeforeAll
-  public static void beforeAll() {
+  public static void beforeAll(Vertx vertx, VertxTestContext testContext) {
     File tmpDir = new File(System.getProperty("java.io.tmpdir") + File.separator + "obis-test");
     dbPath = new File(tmpDir.getAbsolutePath() + File.separator + "db-" + Generators.timeBasedGenerator().generate());
-  }
-
-  @BeforeEach
-  public void beforeEach(Vertx vertx, VertxTestContext testContext) {
     vertx.deployVerticle(new EmbeddedDb(), new DeploymentOptions()
         .setConfig(new JsonObject()
           .put("bindIp", BIND_IP)
           .put("port", PORT)
           .put("path", dbPath.getAbsolutePath())),
       deployId -> {
-        Logger.info("Deployed DB {}", deployId);
+        Logger.info("Deployed DB {}", deployId.result());
         client = MongoClient.createNonShared(vertx,
           new JsonObject()
             .put("host", BIND_IP)
@@ -63,16 +62,17 @@ public class EmbeddedDbWithPathTest {
       });
   }
 
-  @AfterEach
-  public void AfterEach(Vertx vertx) {
-    vertx.close();
-  }
-
   @AfterAll
-  static public void afterAll() throws IOException {
-    if (dbPath.exists()) {
-      FileUtils.deleteDirectory(dbPath);
-    }
+  public static void afterAll(Vertx vertx) {
+    vertx.close(result -> {
+      if (dbPath.exists()) {
+        try {
+          FileUtils.deleteDirectory(dbPath);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    });
   }
 
   @Test
@@ -105,6 +105,5 @@ public class EmbeddedDbWithPathTest {
         }
         testContext.completeNow();
       });
-
   }
 }
