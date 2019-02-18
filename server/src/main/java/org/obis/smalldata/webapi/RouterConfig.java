9269738437d8860fc80465f5053f4fe00caf2b4e
kurt.sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-02-18 16:33:44 +0100

    put handler logic in separate classes
    + new class for status handling
    + use a map to link operationId with handler using method references

--- server/src/main/java/org/obis/smalldata/webapi/RouterConfig.java
+++ server/src/main/java/org/obis/smalldata/webapi/RouterConfig.java
@@ -1,13 +1,21 @@
 package org.obis.smalldata.webapi;
 
+import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
 import io.vertx.ext.web.handler.StaticHandler;
 
+import java.util.Map;
 import java.util.function.Consumer;
 
+import static java.util.Map.entry;
+
 class RouterConfig {
+  private final Map<String, Handler<RoutingContext>> handlers = Map.ofEntries(
+    entry("getStatus", StatusHandler::status)
+  );
   private final Consumer<Router> completionHandler;
 
   RouterConfig(Consumer<Router> completionHandler) {
@@ -15,10 +23,8 @@ class RouterConfig {
   }
 
   void invoke(final OpenAPI3RouterFactory routerFactory) {
-    routerFactory.addHandlerByOperationId("getStatus",
-      context -> context.response()
-        .putHeader("content-type", "application/json")
-        .end(new JsonObject().put("title", "Small Data Status").encode()));
+    handlers.forEach(routerFactory::addHandlerByOperationId);
+
     Router router = routerFactory.getRouter();
     router.get("/*").handler(StaticHandler.create());
     completionHandler.accept(router);
