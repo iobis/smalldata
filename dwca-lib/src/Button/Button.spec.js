384343f89a6add0e25516d657bae7aa36fd2ca9c
Oleg Tsvetkov <olecve@gmail.com>
2019-02-20 22:34:05 +0200

    Update test accodirng to new changes

--- dwca-lib/src/Button/Button.spec.js
+++ dwca-lib/src/Button/Button.spec.js
@@ -2,10 +2,12 @@ import Button from './Button'
 import renderer from 'react-test-renderer'
 import React from 'react'
 
+const noop = ()=>{}
+
 describe('Button', () => {
   it('renders correctly', () => {
-    const component = renderer.create(<Button />)
-    let tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(renderer.create(<Button/>).toJSON()).toMatchSnapshot()
+    expect(renderer.create(<Button onClick={noop}/>).toJSON()).toMatchSnapshot()
+    expect(renderer.create(<Button>click me</Button>).toJSON()).toMatchSnapshot()
   })
 })
