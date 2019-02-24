c4495c9edaf208b1081b26a0dc373edb10a6e8fe
Oleg Tsvetkov <olecve@gmail.com>
2019-02-24 22:00:34 +0200

    Add user icon next to logout

--- occurrences-app/src/App.js
+++ occurrences-app/src/App.js
@@ -32,7 +32,7 @@ export default function App() {
           </div>
           <div className="navbar-end">
             <a className="navbar-item" onClick={() => console.log('TBD: logout clicked')}>
-              logout
+              <span className="icon"><i className="fas fa-user"></i></span>&nbsp;logout
             </a>
           </div>
         </div>
