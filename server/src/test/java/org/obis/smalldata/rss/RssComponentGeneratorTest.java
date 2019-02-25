85cd7bb166401b0272aed0c7327c62c650d09ec3
Oleg Tsvetkov <olecve@gmail.com>
2019-02-25 23:28:44 +0200

    Remove dead code

--- server/src/test/java/org/obis/smalldata/rss/RssComponentGeneratorTest.java
+++ server/src/test/java/org/obis/smalldata/rss/RssComponentGeneratorTest.java
@@ -1,6 +1,5 @@
 package org.obis.smalldata.rss;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.obis.smalldata.rss.model.Channel;
