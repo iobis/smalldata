8e569841c8c5d57f3021f6e1325cb95374c97f1a
Oleg Tsvetkov <olecve@gmail.com>
2019-03-01 22:35:26 +0200

    Add nl translation resource
    We don't have any ui to switch locale, so it can be done programatticaly in the config
    at the moment. Later we can integrate browser locale identification or url parameter.
    The main goal is to keep all strings in resource files

--- occurrences-app/src/i18n/i18n.js
+++ occurrences-app/src/i18n/i18n.js
@@ -1,9 +1,10 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
 import en from './en'
+import i18n from 'i18next'
+import nl from './nl'
+import { initReactI18next } from 'react-i18next'
 
 const resources = {
-  en
+  en, nl
 }
 
 i18n
