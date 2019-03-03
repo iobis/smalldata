dbe5b2dc5e00753e7598dce8b4fbe5be871a43bb
Oleg Tsvetkov <olecve@gmail.com>
2019-03-03 22:11:41 +0200

    Rename handler function to emphasize intend

--- occurrences-app/src/layout/Navbar.js
+++ occurrences-app/src/layout/Navbar.js
@@ -9,15 +9,13 @@ export default function Navbar() {
   const { t } = useTranslation()
   const [navbarMenuActive, setNavbarMenuActive] = useState(false)
   const menuRef = useRef()
-
-  const onNavbarItemClick = () => setNavbarMenuActive(false)
-
-  useOnClickOutside(menuRef, () => setNavbarMenuActive(false))
+  const hideNavbarMenu = () => setNavbarMenuActive(false)
+  useOnClickOutside(menuRef, hideNavbarMenu)
 
   return (
     <nav className="navbar is-info" role="navigation" ref={menuRef} aria-label="main navigation">
       <div className="navbar-brand">
-        <Link to="/input-data" className="navbar-item">
+        <Link onClick={hideNavbarMenu} to="/input-data" className="navbar-item">
           <p style={{ 'width': 112, 'fontSize': 26, 'fontWeight': 'bold' }}>OBIS</p>
         </Link>
         <a
@@ -31,10 +29,10 @@ export default function Navbar() {
       </div>
       <div className={classNames('navbar-menu', { 'is-active': navbarMenuActive })}>
         <div className="navbar-start">
-          <NavbarItem onClick={onNavbarItemClick} to="/input-data">
+          <NavbarItem onClick={hideNavbarMenu} to="/input-data">
             {t('navbar.inputData')}
           </NavbarItem>
-          <NavbarItem onClick={onNavbarItemClick} to="/help">
+          <NavbarItem onClick={hideNavbarMenu} to="/help">
             {t('navbar.help')}
           </NavbarItem>
         </div>
