cfaaa5e053b5dd6bb125672e74ca7d9a0be51e0a
Oleg Tsvetkov <olecve@gmail.com>
2019-03-04 22:19:54 +0200

    Add EN translation for input page

--- occurrences-app/src/i18n/en.js
+++ occurrences-app/src/i18n/en.js
@@ -1,7 +1,16 @@
 export default {
   translation: {
-    'navbar.inputData': 'INPUT DATA',
-    'navbar.help':      'HELP',
-    'navbar.logout':    'logout'
+    'common.copy':                    'Copy',
+    'common.edit':                    'Edit',
+    'inputData.copyPreviousHeader':   'copy from previous entries',
+    'inputData.inputNew':             'INPUT NEW OCCURRENCE',
+    'inputData.or':                   'OR',
+    'inputData.table.dataset':        'Dataset',
+    'inputData.table.dateAdded':      'Date Added',
+    'inputData.table.occurrenceDate': 'Occurrence Date',
+    'inputData.table.scientificName': 'Scientific Name',
+    'navbar.help':                    'HELP',
+    'navbar.inputData':               'INPUT DATA',
+    'navbar.logout':                  'logout'
   }
 }
--- occurrences-app/src/pages/InputDataPage.js
+++ occurrences-app/src/pages/InputDataPage.js
@@ -1,41 +1,53 @@
 import React from 'react'
+import Divider from '../atom/Divider'
+import { useTranslation } from 'react-i18next'
 
 export default function InputDataPage() {
+  const { t } = useTranslation()
+
   return (
-    <div className="container is-fluid has-text-centered">
-      <a className="button is-info">INPUT NEW OCCURRENCE</a>
-      <hr/>
-      <h3>copy from previous entries</h3>
-      <table className="table is-striped is-hoverable is-fullwidth">
-        <thead>
-        <tr>
-          <th></th>
-          <th>Date Added</th>
-          <th>Scientific Name</th>
-          <th>Dataset</th>
-          <th>Occurrence Date</th>
-          <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td><a className="button is-info">edit</a></td>
-          <td>01/12/2011</td>
-          <td>Abra Alba</td>
-          <td>NPPSD Short-tailed Albatross Sightings</td>
-          <td>09/12/2001</td>
-          <td><a className="button is-info">copy</a></td>
-        </tr>
-        <tr>
-          <td><a className="button is-info">edit</a></td>
-          <td>01/12/2011</td>
-          <td>Abra Alba</td>
-          <td>NPPSD Short-tailed Albatross Sightings</td>
-          <td>09/12/2001</td>
-          <td><a className="button is-info">copy</a></td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      <section className="section">
+        <div className="container is-fluid has-text-centered">
+          <a className="button is-info">{t('inputData.inputNew')}</a>
+        </div>
+      </section>
+      <Divider>{t('inputData.or')}</Divider>
+      <section className="section">
+        <div className="container is-fluid has-text-centered">
+          <h3>{t('inputData.copyPreviousHeader')}</h3>
+          <table className="table is-striped is-hoverable is-fullwidth">
+            <thead>
+            <tr>
+              <th></th>
+              <th>{t('inputData.table.dateAdded')}</th>
+              <th>{t('inputData.table.scientificName')}</th>
+              <th>{t('inputData.table.dataset')}</th>
+              <th>{t('inputData.table.occurrenceDate')}</th>
+              <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td><a className="button is-info">{t('common.edit')}</a></td>
+              <td>01/12/2011</td>
+              <td>Abra Alba</td>
+              <td>NPPSD Short-tailed Albatross Sightings</td>
+              <td>09/12/2001</td>
+              <td><a className="button is-info">{t('common.copy')}</a></td>
+            </tr>
+            <tr>
+              <td><a className="button is-info">{t('common.edit')}</a></td>
+              <td>01/12/2011</td>
+              <td>Abra Alba</td>
+              <td>NPPSD Short-tailed Albatross Sightings</td>
+              <td>09/12/2001</td>
+              <td><a className="button is-info">{t('common.copy')}</a></td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
   )
 }
