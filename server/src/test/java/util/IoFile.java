b9362a4f8e4ac3ee6acdb4a57dfb54392f859c44
Oleg Tsvetkov <olecve@gmail.com>
2019-03-09 12:34:44 +0200

    Provide string from IoFIle helper and update unit tests

--- server/src/test/java/util/IoFile.java
+++ server/src/test/java/util/IoFile.java
@@ -4,20 +4,19 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class IoFile {
 
-  public static void doWithFileContent(String strPath, Consumer<String> stringConsumer) {
+  public static String loadFromResources(String strPath) {
     try {
       var path = Paths.get(IoFile.class.getClassLoader().getResource(strPath).toURI());
       var lines = Files.lines(path);
-      var xmlExpected = lines.collect(Collectors.joining("\n"));
+      var text = lines.collect(Collectors.joining("\n"));
       lines.close();
-      stringConsumer.accept(xmlExpected);
+      return text;
     } catch (IOException | URISyntaxException e) {
-      e.printStackTrace();
+      throw new RuntimeException(e);
     }
   }
 }
