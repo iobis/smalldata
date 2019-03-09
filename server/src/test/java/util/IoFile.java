66706895ea70b058d99caaaf93e72f452602c519
Oleg Tsvetkov <olecve@gmail.com>
2019-03-09 17:19:48 +0200

    Fix issue reported by PMD

--- server/src/test/java/util/IoFile.java
+++ server/src/test/java/util/IoFile.java
@@ -9,7 +9,7 @@ public class IoFile {
 
   public static String loadFromResources(String strPath) {
     try {
-      var path = Paths.get(IoFile.class.getClassLoader().getResource(strPath).toURI());
+      var path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(strPath).toURI());
       var bytes = Files.readAllBytes(path);
       return new String(bytes);
     } catch (IOException | URISyntaxException e) {
