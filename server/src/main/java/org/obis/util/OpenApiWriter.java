da1b0bac2eccb13f875da2ddaf9e363f814e9797
Oleg Tsvetkov <olecve@gmail.com>
2019-03-10 22:40:45 +0200

    Remove dead code

--- server/src/main/java/org/obis/util/OpenApiWriter.java
+++ server/src/main/java/org/obis/util/OpenApiWriter.java
@@ -1,6 +1,5 @@
 package org.obis.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
@@ -14,16 +13,6 @@ import static org.pmw.tinylog.Logger.info;
 
 public class OpenApiWriter {
   private final YAMLMapper yamlMapper = new YAMLMapper().enable(YAMLGenerator.Feature.MINIMIZE_QUOTES);
-
-  void writeAsString(Map<String, Map<String, Object>> api, String extensionName) {
-    try {
-      info(yamlMapper.writeValueAsString(
-        Map.of(extensionName,
-          Map.of("properties", api))));
-    } catch (JsonProcessingException e) {
-      error(e);
-    }
-  }
 
   void writeAsFile(Map<String, Map<String, Object>> api, String extensionName, String targetPath) {
     try {
