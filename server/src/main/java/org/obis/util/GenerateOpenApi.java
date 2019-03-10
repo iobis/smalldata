aa0069c1798034e1ce068c1b2cbb8efaa308309c
Oleg Tsvetkov <olecve@gmail.com>
2019-03-10 21:44:55 +0200

    Transform constant to static final field

--- server/src/main/java/org/obis/util/GenerateOpenApi.java
+++ server/src/main/java/org/obis/util/GenerateOpenApi.java
@@ -15,6 +15,12 @@ import static org.pmw.tinylog.Logger.error;
 
 public class GenerateOpenApi {
   private static final NamespaceMapper NS_MAPPER = NamespaceMapper.INSTANCE;
+  private static final Map<String, String> FILES_TO_PROCESS = Map.of(
+    "http://rs.gbif.org/core/dwc_occurrence_2015-07-02.xml", "Occurrence",
+    "http://rs.gbif.org/core/dwc_taxon_2015-04-24.xml", "Taxon",
+    "http://rs.gbif.org/core/dwc_event_2016_06_21.xml", "Event",
+    "http://rs.gbif.org/extension/obis/extended_measurement_or_fact.xml", "EMOF"
+  );
 
   private final DarwinCoreExtensionReader xmlReader = new DarwinCoreExtensionReader();
   private final OpenApiModelConstructor apiConstructor = new OpenApiModelConstructor();
@@ -22,14 +28,8 @@ public class GenerateOpenApi {
   private final List<Function<Map<String, Map<String, Object>>, Map<String, Map<String, Object>>>> customizers =
     List.of(new TypeMapper(), new CustomFieldMerger());
   private final String targetPath;
-  private final Map<String, String> filesToProcess = Map.of(
-    "http://rs.gbif.org/core/dwc_occurrence_2015-07-02.xml", "Occurrence",
-    "http://rs.gbif.org/core/dwc_taxon_2015-04-24.xml", "Taxon",
-    "http://rs.gbif.org/core/dwc_event_2016_06_21.xml", "Event",
-    "http://rs.gbif.org/extension/obis/extended_measurement_or_fact.xml", "EMOF"
-  );
 
-  GenerateOpenApi(String targetPath) {
+  private GenerateOpenApi(String targetPath) {
     this.targetPath = targetPath;
     NS_MAPPER.put("purl", "http://purl.org/dc/terms/");
     NS_MAPPER.put("dwcg", "http://rs.tdwg.org/dwc/terms/");
@@ -52,7 +52,7 @@ public class GenerateOpenApi {
   }
 
   private void start() {
-    filesToProcess.entrySet().forEach(this::processXml);
+    FILES_TO_PROCESS.entrySet().forEach(this::processXml);
   }
 
   public static void main(String... args) {
