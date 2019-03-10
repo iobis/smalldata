73e639befa948e9cac9489f00e16c33665a77df9
Oleg Tsvetkov <olecve@gmail.com>
2019-03-10 18:24:27 +0200

    Remove commented code

--- server/src/main/java/org/obis/smalldata/webapi/OccurrenceHandler.java
+++ server/src/main/java/org/obis/smalldata/webapi/OccurrenceHandler.java
@@ -3,15 +3,16 @@ package org.obis.smalldata.webapi;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
-import org.pmw.tinylog.Logger;
+
+import static org.pmw.tinylog.Logger.info;
 
 public class OccurrenceHandler {
 
   static void post(RoutingContext context) {
-    Logger.info("context: {}", context.request());
-    context.response()
+    info("context: {}", context.request());
+    context
+      .response()
       .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-      //.putHeader(HttpHeaders.TRANSFER_ENCODING, "chunked")
       .end(new JsonObject().put("occurenceID", "some ID").encode());
   }
 }
