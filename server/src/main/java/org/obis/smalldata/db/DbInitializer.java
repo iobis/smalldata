c6d17fdb0acf3f34c213cd22098fcc3906de93f5
kurt-o-sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-03-12 00:11:06 +0100

    setup bulkwrite to add mockdata

--- server/src/main/java/org/obis/smalldata/db/DbInitializer.java
+++ server/src/main/java/org/obis/smalldata/db/DbInitializer.java
@@ -1,16 +1,31 @@
 package org.obis.smalldata.db;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vertx.core.Handler;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.BulkOperation;
 import io.vertx.ext.mongo.MongoClient;
+import org.obis.smalldata.util.IoFile;
 import org.pmw.tinylog.Logger;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.pmw.tinylog.Logger.info;
+import static org.pmw.tinylog.Logger.warn;
 
 public class DbInitializer {
 
-  private UniqueIdGenerator uniqueIdGenerator;
-  private MongoClient client;
+  private final UniqueIdGenerator uniqueIdGenerator;
+  private final MongoClient client;
 
   DbInitializer(MongoClient client) {
     this.client = client;
@@ -19,7 +34,7 @@ public class DbInitializer {
 
   public void newUser(String userId) {
     client.find(Collections.USERS.dbName(), new JsonObject().put("userid", userId),
-      arUserId -> Logger.info("userID: {}", arUserId.result()));
+      arUserId -> info("userID: {}", arUserId.result()));
     client.insert(Collections.USERS.dbName(), new JsonObject()
         .put("userid", userId)
         .put("username", "admin")
@@ -27,33 +42,47 @@ public class DbInitializer {
         .put("roles", new JsonArray().add("node admin")),
       arObjectId -> {
         var objectId = arObjectId.result();
-        Logger.info("added object {}", objectId);
+        info("added object {}", objectId);
         client.find("users", new JsonObject().put("userid", userId),
-          arRecord -> Logger.info(arRecord.result()));
+          arRecord -> info(arRecord.result()));
       });
   }
 
-  private void addCollections() {
-    uniqueIdGenerator.consumeNewId(Collections.USERS.dbName(), "userid", this::newUser);
-    List.of(new String[]{Collections.DWCADOCS.dbName(), "dwcaid"},
-      new String[]{Collections.DWCADOCS.dbName(), "datasetid"},
-      new String[]{Collections.DATASETS.dbName(), "datasetid"})
+  private void addIndices() {
+    //uniqueIdGenerator.consumeNewId(Collections.USERS.dbName(), "_ref", this::newUser);
+    List.of(new String[]{Collections.DWCADOCS.dbName(), "_ref"},
+      new String[]{Collections.DWCADOCS.dbName(), "dataset_ref"},
+      new String[]{Collections.DATASETS.dbName(), "_ref"})
       .forEach(entry ->
         client.createIndex(entry[0], new JsonObject().put(entry[1], 1)
-            .put("collation", Const.INSTANCE.collation), x -> Logger.info("created index '{}'", entry[1])));
+          .put("collation", Const.INSTANCE.collation)
+          .put("background", true), x -> info("created index '{}.{}'", entry[0], entry[1])));
+  }
+
+  private void addMockData() {
+    var operations = BulkOperationUtil.createOperationsFromJson(IoFile.loadFromResources("mockdata/users.json"));
+    client.bulkWrite("users", operations,
+      arClient -> warn("write result: {}",arClient.result().toJson()));
   }
 
   void mockData() {
+    warn("Adding mock data!");
+    client.getCollections(arColls -> {
+      if (arColls.result() != null)
+        arColls.result().forEach(coll -> client.dropCollection(coll, ar ->
+          info("Dropped collection {}", coll)));
+    });
+    addMockData();
   }
 
-  void createCollections() {
+  void setupCollections() {
     client.getCollections(arColls -> {
       var colls = arColls.result();
       if (colls.isEmpty()) {
-        Logger.info("No data found, creating collections");
-        addCollections();
+        info("No data found, creating indices");
+        addIndices();
       } else if (colls.contains("users") && colls.contains("datasets")) {
-        Logger.info("Found collections {} - OK", colls);
+        info("Found collections {} - OK", colls);
       } else {
         Logger.warn("Found not all collections {} - No clue what to do now", colls);
       }
---
+++ server/src/test/java/org/obis/smalldata/db/MockDataTest.java
@@ -0,0 +1,66 @@
+package org.obis.smalldata.db;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.BulkOperation;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.junit5.Timeout;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.pmw.tinylog.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.pmw.tinylog.Logger.info;
+import static org.pmw.tinylog.Logger.warn;
+
+@ExtendWith(VertxExtension.class)
+public class MockDataTest {
+
+  private static final String BIND_IP = "localhost";
+  private static final int PORT = 12345;
+
+  private MongoClient client;
+
+  @BeforeEach
+  public void beforeEach(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new EmbeddedDb(), new DeploymentOptions()
+        .setConfig(new JsonObject()
+          .put("bindIp", BIND_IP)
+          .put("port", PORT)),
+      testContext.succeeding(deployId -> {
+        info("Deployed DB {}", deployId);
+        client = MongoClient.createNonShared(vertx,
+          new JsonObject()
+            .put("host", BIND_IP)
+            .put("port", PORT));
+        info("Running client {}", client);
+        testContext.completeNow();
+      }));
+  }
+
+  @Test
+  @DisplayName("read mock data from json and add to db")
+  @Timeout(value = 5, timeUnit = TimeUnit.SECONDS)
+  public void bulkWrite(VertxTestContext testContext) {
+    var operations = BulkOperationUtil.createOperationsFromFile("mockdata/testusers.json");
+    client.bulkWrite("users", operations,
+      arClient -> {
+        client.find("users", new JsonObject(), ar -> {
+          assertTrue(ar.result().size() == 2);
+          testContext.completeNow();
+        });
+    });
+  }
+
+  @AfterEach
+  @DisplayName("Check that the verticle is still there")
+  void lastChecks(Vertx vertx) {
+    assertTrue(!vertx.deploymentIDs().isEmpty() &&
+      vertx.deploymentIDs().size()==1);
+  }
+}
