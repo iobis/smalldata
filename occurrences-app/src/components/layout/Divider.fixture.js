3d37b881f36e1e365ad786b38ad6cc56bfe7cb34
Oleg Tsvetkov <olecve@gmail.com>
2019-03-16 19:01:07 +0200

    Update fixture to have padding-top

--- occurrences-app/src/components/layout/Divider.fixture.js
+++ occurrences-app/src/components/layout/Divider.fixture.js
@@ -1,7 +1,12 @@
 import Divider from './Divider'
+import React from 'react'
 
 export default {
-  component: Divider,
+  component: props => (
+    <div style={{ 'padding-top': '1rem' }}>
+      <Divider {...props}/>
+    </div>
+  ),
   props:     {
     children: 'OR'
   }
