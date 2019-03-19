9d1e413047a6f58aceb169686f8f8300f3e05410
Oleg Tsvetkov <olecve@gmail.com>
2019-03-19 21:13:46 +0200

    Update name of the fixture

--- occurrences-app/src/components/pages/Occurrence/StepHeader.fixture.js
+++ occurrences-app/src/components/pages/Occurrence/StepHeader.fixture.js
@@ -5,12 +5,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 library.add(faCheckCircle)
 
-export default {
+export default [{
   component: StepHeader,
+  name:      'confirmed step',
   props:     {
     dataDescription: 'Using Data',
     selectedData:    'HAB Region 2: Occurrences of harmful (toxic) algal taxa within an area of interest to El Salvador compiled as part of a literature search project.',
     stepDescription: 'Choose the dataset for adding observations',
     stepTitle:       '1 - Select Dataset'
   }
-}
+}]
