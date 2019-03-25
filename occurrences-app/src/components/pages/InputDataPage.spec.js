efebd3517dba780bb0ca8fc9426ec274841c6bb3
Oleg Tsvetkov <olecve@gmail.com>
2019-03-25 23:02:28 +0200

    Fix InputDataPage test

--- occurrences-app/src/components/pages/InputDataPage.spec.js
+++ occurrences-app/src/components/pages/InputDataPage.spec.js
@@ -1,9 +1,10 @@
 import InputDataPage from './InputDataPage'
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('InputDataPage', () => {
   it('renders correctly /', () => {
-    expect(renderer.create(<InputDataPage/>).toJSON()).toMatchSnapshot()
+    expect(renderer.create(<MemoryRouter><InputDataPage/></MemoryRouter>).toJSON()).toMatchSnapshot()
   })
 })
