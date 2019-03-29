f9aeee414b4601a439b98d5ef26eec7340a2d5f7
kurt.sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-03-29 23:00:21 +0100

    Merge branch 'master' into 24.start-in-memory-mongodb-if-no-config
    # Conflicts:
    #	.idea/inspectionProfiles/Project_Default.xml

--- server/src/test/java/org/obis/smalldata/db/SecureRandomIdTest.java
+++ server/src/test/java/org/obis/smalldata/db/SecureRandomIdTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.pmw.tinylog.Logger.info;
 
 public class SecureRandomIdTest {
@@ -13,7 +13,7 @@ public class SecureRandomIdTest {
   public void testRandomId() {
     Stream.generate(SecureRandomId::generate).limit(10).forEach(srid -> {
       info(srid);
-      assertSame(srid.length(), 15);
+      assertEquals(srid.length(), 15);
     });
   }
 }
