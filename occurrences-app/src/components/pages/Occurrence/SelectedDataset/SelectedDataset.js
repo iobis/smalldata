b90fe2272ec753dd9b01f1d5990ae59ebf996c87
Oleg Tsvetkov <olecve@gmail.com>
2019-04-03 23:49:21 +0300

    Use data to generate components

--- occurrences-app/src/components/pages/Occurrence/SelectedDataset/SelectedDataset.js
+++ occurrences-app/src/components/pages/Occurrence/SelectedDataset/SelectedDataset.js
@@ -2,7 +2,26 @@ import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 
 export default function SelectedDataset() {
-  const [datasetId, setDataSetId] = useState(null)
+  const datasetOptions = [{
+    id:          0,
+    description: 'HAB Region 2: Occurrences of harmful (toxic) algal taxa within an area of interest to El Salvador compiled as part of a literature search project.'
+  }, {
+    id:          1,
+    description: 'NPPSD Short-tailed Albatross Sightings'
+  }, {
+    id:          2,
+    description: 'PANGAEA - Data from Christian-Albrechts-University Kiel'
+  }, {
+    id:          3,
+    description: 'NSIS: List of marine benthic algae from Magdalen Islands, Quebec as recorded in 1979'
+  }, {
+    id:          4,
+    description: 'Seguimiento de 10 crías de tortuga boba nacidas en 2016 en el litoral valenciano, en el marco del Proyecto LIFE 15 IPE ES 012 (aggregated per 1-degree cell)'
+  }, {
+    id:          5,
+    description: 'Waved Albatross Tracking (aggregated per 1-degree cell)'
+  }]
+  const [selectedDatasetId, setSeletedDatasetId] = useState(datasetOptions[0].id)
 
   return (
     <div className="selected-dataset is-fluid">
@@ -10,29 +29,13 @@ export default function SelectedDataset() {
         <div className="column">
           <table className="table is-striped is-fullwidth">
             <tbody>
-            <DatasetOption
-              checked={true}
-              onClick={() => setDataSetId(0)}
-              description="HAB Region 2: Occurrences of harmful (toxic) algal taxa within an area of interest to El Salvador compiled as part of a literature search project."/>
-            <DatasetOption
-              checked={false}
-              onClick={() => setDataSetId(1)} description="NPPSD Short-tailed Albatross Sightings"/>
-            <DatasetOption
-              checked={false}
-              onClick={() => setDataSetId(2)}
-              description="PANGAEA - Data from Christian-Albrechts-University Kiel"/>
-            <DatasetOption
-              checked={false}
-              onClick={() => setDataSetId(3)}
-              description="NSIS: List of marine benthic algae from Magdalen Islands, Quebec as recorded in 1979"/>
-            <DatasetOption
-              checked={false}
-              onClick={() => setDataSetId(4)}
-              description="Seguimiento de 10 crías de tortuga boba nacidas en 2016 en el litoral valenciano, en el marco del Proyecto LIFE 15 IPE ES 012 (aggregated per 1-degree cell)"/>
-            <DatasetOption
-              checked={false}
-              onClick={() => setDataSetId(5)}
-              description="Waved Albatross Tracking (aggregated per 1-degree cell)"/>
+            {datasetOptions.map(props => (
+              <DatasetOption
+                key={props.id}
+                onClick={() => setSeletedDatasetId(props.id)}
+                checked={props.id === selectedDatasetId}
+                {...props}
+              />))}
             </tbody>
           </table>
         </div>
