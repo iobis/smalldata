7a0b593aeff043a07274cb060e0aa9206bfdae4b
Oleg Tsvetkov <olecve@gmail.com>
2019-04-04 23:23:03 +0300

    Move hardoced datasets to server sournce code for now
    later we need to replace it by the real call to the api

--- occurrences-app/src/components/pages/Occurrence/OccurrenceForm.js
+++ occurrences-app/src/components/pages/Occurrence/OccurrenceForm.js
@@ -3,27 +3,10 @@ import ConfirmedStepHeader from './ConfirmedStepHeader'
 import NotConfirmedStepHeader from './NotConfirmedStepHeader'
 import React, { useState } from 'react'
 import SelectedDataset from './SelectedDataset/SelectedDataset'
+import { getDatasetMock } from '../../../clients/server'
 
 export default function OccurrenceForm() {
-  const datasets = [{
-    id:          0,
-    description: 'HAB Region 2: Occurrences of harmful (toxic) algal taxa within an area of interest to El Salvador compiled as part of a literature search project.'
-  }, {
-    id:          1,
-    description: 'NPPSD Short-tailed Albatross Sightings'
-  }, {
-    id:          2,
-    description: 'PANGAEA - Data from Christian-Albrechts-University Kiel'
-  }, {
-    id:          3,
-    description: 'NSIS: List of marine benthic algae from Magdalen Islands, Quebec as recorded in 1979'
-  }, {
-    id:          4,
-    description: 'Seguimiento de 10 crías de tortuga boba nacidas en 2016 en el litoral valenciano, en el marco del Proyecto LIFE 15 IPE ES 012 (aggregated per 1-degree cell)'
-  }, {
-    id:          5,
-    description: 'Waved Albatross Tracking (aggregated per 1-degree cell)'
-  }]
+  const datasets = getDatasetMock()
   const [selectedDataset, setSelectedDataset] = useState(datasets[0])
   const [activeStepIndex, setActiveStepIndex] = useState(0)
 
