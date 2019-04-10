23bdb060da8970706e122c7fa1bbd08c6be91bb3
Oleg Tsvetkov <olecve@gmail.com>
2019-04-10 20:54:41 +0300

    Increase timeout for the test

--- server/src/test/java/org/obis/smalldata/db/DemoModeTest.java
+++ server/src/test/java/org/obis/smalldata/db/DemoModeTest.java
@@ -40,7 +40,7 @@ public class DemoModeTest {
 
   @Test
   @DisplayName("read default mock data")
-  @Timeout(value = 5, timeUnit = TimeUnit.SECONDS)
+  @Timeout(value = 8, timeUnit = TimeUnit.SECONDS)
   void findData(VertxTestContext testContext) throws InterruptedException {
     TimeUnit.MILLISECONDS.sleep(500);
     client.find(
