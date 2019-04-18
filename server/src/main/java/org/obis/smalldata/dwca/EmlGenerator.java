4470fd888bc7a216b6613de97f4d0cc55a89bdb4
Kurt Sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-04-18 12:26:03 +0200

    move actual logic from test class to main EmlGenerator class

--- server/src/main/java/org/obis/smalldata/dwca/EmlGenerator.java
+++ server/src/main/java/org/obis/smalldata/dwca/EmlGenerator.java
@@ -1,4 +1,48 @@
 package org.obis.smalldata.dwca;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import io.vertx.core.json.JsonObject;
+import lombok.Value;
+import org.obis.smalldata.dwca.xmlmodel.eml.Dataset;
+import org.obis.smalldata.dwca.xmlmodel.eml.Eml;
+
+import java.util.Optional;
+
+import static org.pmw.tinylog.Logger.error;
+
 class EmlGenerator {
+
+  private static XmlMapper xmlMapper = new XmlMapper();
+
+  EmlGenerator() {
+    xmlMapper.setDefaultUseWrapper(false);
+    xmlMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+  }
+
+  Optional<DataSetEmlMap> generate(JsonObject json) {
+    return this.generate(json.mapTo(Dataset.class));
+  }
+
+  Optional<DataSetEmlMap> generate(Dataset dataset) {
+    return this.generate(Eml.builder().dataset(dataset).build());
+  }
+
+  Optional<DataSetEmlMap> generate(Eml eml) {
+    try {
+      return Optional.of(new DataSetEmlMap(eml.getDataset().getId(),
+        xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(eml)));
+    } catch (JsonProcessingException e) {
+      error(e.getMessage());
+      return Optional.empty();
+    }
+  }
+
+  @Value
+  static class DataSetEmlMap {
+    private final String id;
+    private final String eml;
+  }
 }
+
--- server/src/test/java/org/obis/smalldata/dwca/EmlGeneratorTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/EmlGeneratorTest.java
@@ -1,16 +1,9 @@
 package org.obis.smalldata.dwca;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.vertx.core.json.JsonObject;
-import lombok.Value;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.obis.smalldata.dwca.xmlmodel.eml.Dataset;
-import org.obis.smalldata.dwca.xmlmodel.eml.Eml;
 import org.obis.smalldata.util.IoFile;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.diff.DefaultNodeMatcher;
@@ -19,6 +12,7 @@ import org.xmlunit.diff.ElementSelectors;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -27,36 +21,21 @@ import static org.pmw.tinylog.Logger.error;
 public class EmlGeneratorTest {
 
   private static ObjectMapper mapper = new ObjectMapper();
-  private static XmlMapper xmlMapper = new XmlMapper();
-
-  @BeforeEach
-  void setUp() {
-    xmlMapper.setDefaultUseWrapper(false);
-    xmlMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-  }
 
   @Test
   void testSimpleEml() {
+    var generator = new EmlGenerator();
     var datasets = IoFile.loadFromResources("testdata/dwca/datasets.json");
     try {
       var json = (List<Map<String, Object>>) mapper.readValue(datasets,
         new TypeReference<List<Map<String, Object>>>() {});
       json.stream()
         .map(JsonObject::new)
-        .map(ds -> ds.mapTo(Dataset.class))
-        .map(ds -> Eml.builder().dataset(ds).build())
-        .map(eml -> {
-          try {
-            return new DataSetEmlMap(eml.getDataset().getId(),
-              xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(eml));
-          } catch (JsonProcessingException e) {
-            error(e.getMessage());
-            return new DataSetEmlMap("", "");
-          }
-        })
+        .map(generator::generate)
+        .map(Optional::get)
         .forEach(xml -> {
-          var diff = DiffBuilder.compare(xml.eml)
-            .withTest(IoFile.loadFromResources("testdata/dwca/" + xml.id + ".xml"))
+          var diff = DiffBuilder.compare(xml.getEml())
+            .withTest(IoFile.loadFromResources("testdata/dwca/" + xml.getId() + ".xml"))
             .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byName))
             .checkForSimilar()
             .ignoreWhitespace()
@@ -69,11 +48,5 @@ public class EmlGeneratorTest {
       error(e.getMessage());
       fail(e.getMessage());
     }
-  }
-
-  @Value
-  static class DataSetEmlMap {
-    private final String id;
-    private final String eml;
   }
 }
