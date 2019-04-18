d89601f70c5e2bcf22bb6156469cbbd679420583
Oleg Tsvetkov <olecve@gmail.com>
2019-04-19 00:24:50 +0300

    Add devcards

--- occurrences-app/src/components/pages/Occurrence/BasicData/BasicData.fixture.js
+++ occurrences-app/src/components/pages/Occurrence/BasicData/BasicData.fixture.js
@@ -1,5 +1,33 @@
 import BasicData from './BasicData'
 
 export default [{
-  component: BasicData
+  component: BasicData,
+  name:      'no selection step',
+  props:     {
+    onChange:  console.log,
+    basicData: {
+      basisOfRecord:    null,
+      beginDate:        new Date(),
+      endDate:          null,
+      lifestage:        null,
+      occurrenceStatus: null,
+      scientificName:   '',
+      sex:              null
+    }
+  }
+}, {
+  component: BasicData,
+  name:      'with selections',
+  props:     {
+    onChange:  console.log,
+    basicData: {
+      basisOfRecord:    'humanObservation',
+      beginDate:        new Date(),
+      endDate:          new Date(),
+      lifestage:        'adult',
+      occurrenceStatus: 'absent',
+      scientificName:   'ala abra',
+      sex:              'male'
+    }
+  }
 }]
