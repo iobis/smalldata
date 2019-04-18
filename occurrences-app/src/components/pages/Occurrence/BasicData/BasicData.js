03aae6b9777d72e6c6a369c287809e6e91177bfa
Oleg Tsvetkov <olecve@gmail.com>
2019-04-18 18:48:57 +0300

    Keep form selection in component state

--- occurrences-app/src/components/pages/Occurrence/BasicData/BasicData.js
+++ occurrences-app/src/components/pages/Occurrence/BasicData/BasicData.js
@@ -4,6 +4,10 @@ import bulmaCalendar from 'bulma-calendar'
 export default function BasicData() {
   const [beginDate, setBeginDate] = useState(new Date())
   const [endDate, setEndDate] = useState(null)
+  const [occurrenceStatus, setOccurrenceStatus] = useState(null)
+  const [basisOfRecord, setBasisOfRecord] = useState(null)
+  const [sex, setSex] = useState(null)
+  const [lifestage, setLifestage] = useState(null)
 
   return (
     <div className="basic-dataset section is-fluid">
@@ -35,18 +39,53 @@ export default function BasicData() {
       <div className="field">
         <label className="label">Occurrence status</label>
         <div className="control">
-          <InputRadio text="absent" name="occurrence-status"/>
-          <InputRadio text="present" name="occurrence-status"/>
+          <InputRadio
+            checked={occurrenceStatus === 'absent'}
+            name="occurrence-status"
+            onChange={setOccurrenceStatus}
+            text="absent"
+            value="absent"/>
+          <InputRadio
+            checked={occurrenceStatus === 'present'}
+            name="occurrence-status"
+            onChange={setOccurrenceStatus}
+            text="present"
+            value="present"/>
         </div>
       </div>
       <div className="field">
         <label className="label">Basis of record</label>
         <div className="control">
-          <InputRadio text="human observation" name="basis"/>
-          <InputRadio text="fossil specimen" name="basis"/>
-          <InputRadio text="living specimen" name="basis"/>
-          <InputRadio text="machine specimen" name="basis"/>
-          <InputRadio text="preserved specimen" name="basis"/>
+          <InputRadio
+            checked={basisOfRecord === 'humanObservation'}
+            name="basis"
+            onChange={setBasisOfRecord}
+            text="human observation"
+            value="humanObservation"/>
+          <InputRadio
+            checked={basisOfRecord === 'fossilSpecimen'}
+            text="fossil specimen"
+            name="basis"
+            onChange={setBasisOfRecord}
+            value="fossilSpecimen"/>
+          <InputRadio
+            checked={basisOfRecord === 'livingSpecimen'}
+            text="living specimen"
+            name="basis"
+            onChange={setBasisOfRecord}
+            value="livingSpecimen"/>
+          <InputRadio
+            checked={basisOfRecord === 'machineSpecimen'}
+            text="machine specimen"
+            name="basis"
+            onChange={setBasisOfRecord}
+            value="machineSpecimen"/>
+          <InputRadio
+            checked={basisOfRecord === 'preservedSpecimen'}
+            text="preserved specimen"
+            name="basis"
+            onChange={setBasisOfRecord}
+            value="preservedSpecimen"/>
         </div>
       </div>
       <div className="field">
@@ -54,9 +93,24 @@ export default function BasicData() {
           Sex <small>(optional)</small>
         </label>
         <div className="control">
-          <InputRadio text="male" name="sex"/>
-          <InputRadio text="female" name="sex"/>
-          <InputRadio text="hermaphrodite" name="sex"/>
+          <InputRadio
+            checked={sex === 'male'}
+            text="male"
+            name="sex"
+            onChange={setSex}
+            value="male"/>
+          <InputRadio
+            checked={sex === 'female'}
+            text="female"
+            name="sex"
+            onChange={setSex}
+            value="female"/>
+          <InputRadio
+            checked={sex === 'hermaphrodite'}
+            text="hermaphrodite"
+            name="sex"
+            onChange={setSex}
+            value="hermaphrodite"/>
         </div>
       </div>
       <div className="field">
@@ -64,20 +118,47 @@ export default function BasicData() {
           Lifestage <small>(optional)</small>
         </label>
         <div className="control">
-          <InputRadio text="egg" name="lifestage"/>
-          <InputRadio text="eft" name="lifestage"/>
-          <InputRadio text="juvenile" name="lifestage"/>
-          <InputRadio text="adult" name="lifestage"/>
+          <InputRadio
+            checked={lifestage === 'egg'}
+            text="egg"
+            name="lifestage"
+            onChange={setLifestage}
+            value="egg"/>
+          <InputRadio
+            checked={lifestage === 'eft'}
+            text="eft"
+            name="lifestage"
+            onChange={setLifestage}
+            value="eft"/>
+          <InputRadio
+            checked={lifestage === 'juvenile'}
+            text="juvenile"
+            name="lifestage"
+            onChange={setLifestage}
+            value="juvenile"/>
+          <InputRadio
+            checked={lifestage === 'adult'}
+            text="adult"
+            name="lifestage"
+            onChange={setLifestage}
+            value="adult"/>
         </div>
       </div>
     </div>
   )
 }
 
-function InputRadio({ checked, text, name }) {
+function InputRadio({ checked, text, name, onChange, value }) {
   return (
     <>
-      <input className="is-checkradio" id={text} type="radio" name={name} checked={checked}/>
+      <input
+        className="is-checkradio"
+        id={text}
+        type="radio"
+        name={name}
+        checked={checked}
+        onClick={(e) => onChange(e.target.value)}
+        value={value}/>
       <label htmlFor={text}>{text}</label>
     </>
   )
