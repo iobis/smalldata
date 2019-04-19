81be57baac4b6ce54bbb0901bb45c0edca017cb9
Oleg Tsvetkov <olecve@gmail.com>
2019-04-19 23:30:43 +0300

    Fix unit test to work on windows

--- server/src/test/java/org/obis/smalldata/dwca/MetaGeneratorTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/MetaGeneratorTest.java
@@ -1,5 +1,6 @@
 package org.obis.smalldata.dwca;
 
+import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import lombok.AllArgsConstructor;
 import lombok.Value;
@@ -9,46 +10,42 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.pmw.tinylog.Logger.error;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MetaGeneratorTest {
 
   @Test
-  public void testSuccessFullMetaFile() {
+  public void generateXmlSuccessFullMetaFile() throws IOException {
     var processor = new ResourcePathProcessor("mockdata/dwca/ware_hosono-v1.5-csv-namespaced/");
-
     var core = processor.generateMetaConfig(processor.uri("event15584133909797684527.txt"));
     var extensions = List.of("emof15584133909797684527.txt", "occurrence15584133909797684527.txt").stream()
       .map(processor::uri)
       .map(processor::generateMetaConfig)
       .collect(Collectors.toList());
-
     var generator = new MetaGenerator();
-    var xmlFile = generator.generateXml(core, extensions);
-    var resource = Resources.getResource("mockdata/dwca/ware_hosono-v1.5/meta.xml");
-    try {
-      var expected = Files.lines(Paths.get(resource.getPath())).collect(Collectors.joining());
-      var actual = Files.lines(xmlFile.get().toPath()).collect(Collectors.joining());
-      assertEquals(expected.replaceAll("\\s", ""),
-        actual.replaceAll("\\s", "").replaceAll("\\d+\\.txt", ".txt"));
-    } catch (IOException e) {
-      error("failed to assert {}", e.getMessage());
-      fail("failing to read files, cannot assert equality");
-    }
+
+    var generatedXmlFile = generator.generateXml(core, extensions);
+
+    assertTrue(generatedXmlFile.isPresent());
+    var originalXmlUrl = Resources.getResource("mockdata/dwca/ware_hosono-v1.5/meta.xml");
+    var expectedXml = Resources.toString(originalXmlUrl, Charsets.UTF_8);
+    var actualXml = Files.lines(generatedXmlFile.get().toPath()).collect(Collectors.joining());
+    assertEquals(
+      expectedXml.replaceAll("\\s", ""),
+      actualXml.replaceAll("\\s", "").replaceAll("\\d+\\.txt", ".txt"));
   }
 
   @Value
   @AllArgsConstructor
-  static class ResourcePathProcessor {
-    private final Map<String, String> rowTypeMap =
-      Map.of("event15584133909797684527.txt", "http://rs.tdwg.org/dwc/terms/Event",
+  private static class ResourcePathProcessor {
+    private final static Map<String, String> ROW_TYPE_MAP =
+      Map.of(
+        "event15584133909797684527.txt", "http://rs.tdwg.org/dwc/terms/Event",
         "emof15584133909797684527.txt", "http://rs.iobis.org/obis/terms/ExtendedMeasurementOrFact",
         "occurrence15584133909797684527.txt", "http://rs.tdwg.org/dwc/terms/Occurrence");
     private final String baseDir;
@@ -56,10 +53,9 @@ public class MetaGeneratorTest {
     MetaFileConfig generateMetaConfig(URI uri) {
       try {
         var relativeUri = Resources.getResource(baseDir).toURI().relativize(uri).getPath();
-        return new MetaFileConfig(List.of(relativeUri), rowTypeMap.get(relativeUri), uri);
+        return new MetaFileConfig(List.of(relativeUri), ROW_TYPE_MAP.get(relativeUri), uri);
       } catch (URISyntaxException e) {
-        error(e.getMessage());
-        return null;
+        throw new RuntimeException(e);
       }
     }
 
@@ -67,7 +63,7 @@ public class MetaGeneratorTest {
       try {
         return Resources.getResource(baseDir + path).toURI();
       } catch (URISyntaxException e) {
-        return null;
+        throw new RuntimeException(e);
       }
     }
   }
