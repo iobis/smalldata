b223617e91b0768baa6fa7c065950fdbb033fce4
Oleg Tsvetkov <olecve@gmail.com>
2019-04-19 11:09:25 +0300

    Improve test readability
    - move find logic completely to the helper method
    - use beforeAll and afterAll
    - close mongo client at the end of the test

--- server/src/test/java/org/obis/smalldata/dwca/DwcaCsvGeneratorTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/DwcaCsvGeneratorTest.java
@@ -2,14 +2,13 @@ package org.obis.smalldata.dwca;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
 import com.google.common.io.Resources;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -30,62 +29,66 @@ import static org.pmw.tinylog.Logger.warn;
 
 public class DwcaCsvGeneratorTest {
 
-  private final TestDb testDb = new TestDb();
-  private MongoClient mongoClient;
+  private static TestDb testDb;
+  private static MongoClient mongoClient;
 
-  @BeforeEach
-  public void start() {
+  @BeforeAll
+  public static void start() {
+    testDb = new TestDb();
     mongoClient = testDb.init(Vertx.vertx());
   }
 
-  @AfterEach
-  public void stop() {
+  @AfterAll
+  public static void stop() {
     info("shutdown mongo db");
+    mongoClient.close();
     testDb.shutDown();
   }
 
   @Test
-  public void testCsvData() {
-    var future = new CompletableFuture<List<JsonObject>>();
-    doWithDwcaDataset("NnqVLwIyPn-nRkc", res -> future.complete(res.result()));
-    try {
-      var dataset = future.get(200, TimeUnit.MILLISECONDS);
-      var dwcaMap = DwcaData.datasetToDwcaMap(dataset);
-      var dwcCsvWriter = new DwcCsvTable();
-      dwcaMap.forEach((key, value) -> {
-        try {
-          var cvsResourceName = "demodata/dwc/benthic_data_sevastopol-v1.1/" + key + ".txt";
-          var cvsResource = Resources.getResource(cvsResourceName);
-          var cvsContent = Resources.toString(cvsResource, Charsets.UTF_8);
+  public void writeTableToFile() {
+    var datasets = findDatasetsByDatasetRef("NnqVLwIyPn-nRkc");
+    var dwcaMap = DwcaData.datasetToDwcaMap(datasets);
+    var dwcCsvWriter = new DwcCsvTable();
+    dwcaMap.forEach((key, value) -> {
+      try {
+        var cvsResourceName = "demodata/dwc/benthic_data_sevastopol-v1.1/" + key + ".txt";
+        var cvsResource = Resources.getResource(cvsResourceName);
+        var cvsContent = Resources.toString(cvsResource, Charsets.UTF_8);
 
-          File generatedFile = File.createTempFile("obis-iode", key + ".txt");
-          dwcCsvWriter.writeTableToFile(value, generatedFile);
+        File generatedFile = File.createTempFile("obis-iode", key + ".txt");
+        dwcCsvWriter.writeTableToFile(value, generatedFile);
 
-          var nrOfLinesInOriginalCvs = cvsContent.lines().count();
-          var nrOfLinesInGeneratedCvs = Files.lines(generatedFile.toPath()).count();
-          assertEquals(nrOfLinesInOriginalCvs, nrOfLinesInGeneratedCvs);
+        var nrOfLinesInOriginalCvs = cvsContent.lines().count();
+        var nrOfLinesInGeneratedCvs = Files.lines(generatedFile.toPath()).count();
+        assertEquals(nrOfLinesInOriginalCvs, nrOfLinesInGeneratedCvs);
 
-          Set<String> originalHeaders = DwcCsvTable.headersFromFile(new File(cvsResource.getPath()));
-          Set<String> generatedHeaders = DwcCsvTable.headersFromFile(generatedFile);
-          assertTrue(originalHeaders.containsAll(generatedHeaders));
+        Set<String> originalHeaders = DwcCsvTable.headersFromFile(new File(cvsResource.getPath()));
+        Set<String> generatedHeaders = DwcCsvTable.headersFromFile(generatedFile);
+        assertTrue(originalHeaders.containsAll(generatedHeaders));
 
-          if (generatedFile.delete()) {
-            info("deleted {} successfully", generatedFile.getName());
-          } else {
-            warn("could not delete {}", generatedFile.getName());
-          }
-        } catch (IOException e) {
-          error(Throwables.getStackTraceAsString(e));
+        if (generatedFile.delete()) {
+          info("deleted {} successfully", generatedFile.getName());
+        } else {
+          warn("could not delete {}", generatedFile.getName());
         }
-      });
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      error(Throwables.getStackTraceAsString(e));
-    }
+      } catch (IOException e) {
+        error(Throwables.getStackTraceAsString(e));
+      }
+    });
   }
 
-  private void doWithDwcaDataset(String datasetRef, Handler<AsyncResult<List<JsonObject>>> handler) {
-    mongoClient.find("dwcarecords",
-      new JsonObject().put("dataset_ref", datasetRef),
-      handler);
+  private List<JsonObject> findDatasetsByDatasetRef(String datasetRef) {
+    try {
+      var future = new CompletableFuture<List<JsonObject>>();
+      mongoClient.find(
+        "dwcarecords",
+        new JsonObject().put("dataset_ref", datasetRef),
+        ar -> future.complete(ar.result()));
+      return future.get(200, TimeUnit.MILLISECONDS);
+    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      error(Throwables.getStackTraceAsString(e));
+      return ImmutableList.of();
+    }
   }
 }
