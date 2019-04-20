7f77c77bdf058a532e7c9d6d32a3a53576b3ebe3
Oleg Tsvetkov <olecve@gmail.com>
2019-04-20 16:26:18 +0300

    Group decay test to single test block

--- server/src/test/java/org/obis/smalldata/user/BulkinessCalculatorTest.java
+++ server/src/test/java/org/obis/smalldata/user/BulkinessCalculatorTest.java
@@ -10,19 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BulkinessCalculatorTest {
 
   @Test
-  void testDecay() {
-    var calc = new BulkinessCalculator(0.5);
-    assertEquals(5.0 / 16,
-      calc.decay(5, Instant.now().minus(2, ChronoUnit.DAYS)), 1E-6);
-    assertEquals(5.0 / 4,
-      calc.decay(5, Instant.now().minus(1, ChronoUnit.DAYS)), 1E-6);
-    assertEquals(5.0 / 2,
-      calc.decay(5, Instant.now().minus(12, ChronoUnit.HOURS)), 1E-6);
-  }
-
-  @Test
-  void testDecayInitialize() {
-    var calc = new BulkinessCalculator(0.5);
-    assertEquals(0.0, calc.decay(0, Instant.now()));
+  void decay() {
+    var calculator = new BulkinessCalculator(0.5);
+    assertEquals(5.0 / 16, calculator.decay(5, Instant.now().minus(2, ChronoUnit.DAYS)), 1E-6);
+    assertEquals(5.0 / 4, calculator.decay(5, Instant.now().minus(1, ChronoUnit.DAYS)), 1E-6);
+    assertEquals(5.0 / 2, calculator.decay(5, Instant.now().minus(12, ChronoUnit.HOURS)), 1E-6);
+    assertEquals(0.0, calculator.decay(0, Instant.now()));
   }
 }
