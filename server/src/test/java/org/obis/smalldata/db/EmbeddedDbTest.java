615c99d554c98f95caaa6f9b3d74e1ecdd28ce98
Oleg Tsvetkov <olecve@gmail.com>
2019-04-20 12:37:22 +0300

    Close mongo client in all test

--- server/src/test/java/org/obis/smalldata/db/EmbeddedDbTest.java
+++ server/src/test/java/org/obis/smalldata/db/EmbeddedDbTest.java
@@ -7,6 +7,7 @@ import io.vertx.ext.mongo.MongoClient;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.pmw.tinylog.Logger.info;
 
 @ExtendWith(VertxExtension.class)
@@ -23,43 +24,47 @@ public class EmbeddedDbTest {
   private static final String BIND_IP = "localhost";
   private static final int PORT = 12345;
 
-  private MongoClient client;
+  private MongoClient mongoClient;
 
   @BeforeEach
-  public void beforeEach(Vertx vertx, VertxTestContext testContext) {
+  public void setUp(Vertx vertx, VertxTestContext testContext) {
     vertx.deployVerticle(
       new EmbeddedDb(),
       new DeploymentOptions().setConfig(MongoConfigs.ofServer(BIND_IP, PORT)),
       deployId -> {
         info("Deployed DB {}", deployId);
-        client = MongoClient.createNonShared(vertx, MongoConfigs.ofClient(BIND_IP, PORT));
-        info("Running client {}", client);
-        client.createCollection(
+        mongoClient = MongoClient.createNonShared(vertx, MongoConfigs.ofClient(BIND_IP, PORT));
+        info("Running mongoClient {}", mongoClient);
+        mongoClient.createCollection(
           COLLECTION_NAME,
-          res -> {
-            client.insert(COLLECTION_NAME,
+          res -> mongoClient
+            .insert(COLLECTION_NAME,
               new JsonObject()
                 .put("measurementID", 42)
                 .put("measurementUnit", "m2"),
               testContext.succeeding(id -> {
                 info("succeeding {}", id);
                 testContext.completeNow();
-              }));
-          });
+              })));
       });
+  }
+
+  @AfterEach
+  public void tearDown() {
+    mongoClient.close();
   }
 
   @Test
   @DisplayName("returns at least one result")
   @Timeout(value = 5, timeUnit = TimeUnit.SECONDS)
   public void findReturnsAtLeastOneResult(VertxTestContext testContext) {
-    client.find(
+    mongoClient.find(
       COLLECTION_NAME,
       new JsonObject(),
       result -> {
         info("result: {}", result.result());
-        assertTrue(result.succeeded());
-        assertTrue(result.result().size() > 0);
+        assertThat(result.succeeded()).isTrue();
+        assertThat(result.result()).isNotEmpty();
         testContext.completeNow();
       });
   }
@@ -68,13 +73,13 @@ public class EmbeddedDbTest {
   @DisplayName("finds document matching the query")
   @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
   public void findDoc(VertxTestContext testContext) {
-    client.find(
+    mongoClient.find(
       COLLECTION_NAME,
       new JsonObject().put("measurementID", 42),
       result -> {
         info("result: {}", result.result());
-        assertTrue(result.succeeded());
-        assertTrue(result.result().size() > 0);
+        assertThat(result.succeeded()).isTrue();
+        assertThat(result.result()).isNotEmpty();
         testContext.completeNow();
       });
   }
--- server/src/test/java/org/obis/smalldata/db/MockDataTest.java
+++ server/src/test/java/org/obis/smalldata/db/MockDataTest.java
@@ -8,6 +8,7 @@ import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.pmw.tinylog.Logger.info;
 
 @ExtendWith(VertxExtension.class)
@@ -24,20 +25,25 @@ public class MockDataTest {
   private static final String BIND_IP = "localhost";
   private static final int PORT = 12345;
 
-  private MongoClient client;
+  private MongoClient mongoClient;
 
   @BeforeEach
-  public void beforeEach(Vertx vertx, VertxTestContext testContext) {
+  public void setUp(Vertx vertx, VertxTestContext testContext) {
     vertx.sharedData().getLocalMap("settings").put("mode", "TEST");
     vertx.deployVerticle(
       new EmbeddedDb(),
       new DeploymentOptions().setConfig(MongoConfigs.ofServer(BIND_IP, PORT)),
       testContext.succeeding(deployId -> {
         info("Deployed DB {}", deployId);
-        client = MongoClient.createNonShared(vertx, MongoConfigs.ofClient(BIND_IP, PORT));
-        info("Running client {}", client);
+        mongoClient = MongoClient.createNonShared(vertx, MongoConfigs.ofClient(BIND_IP, PORT));
+        info("Running mongoClient {}", mongoClient);
         testContext.completeNow();
       }));
+  }
+
+  @AfterEach
+  public void tearDown() {
+    mongoClient.close();
   }
 
   @Test
@@ -46,14 +52,14 @@ public class MockDataTest {
   public void bulkWrite(VertxTestContext testContext) {
     var operations = BulkOperationUtil.createOperationsFromFile("mockdata/testusers.json");
     Checkpoint checks = testContext.checkpoint(2);
-    client.bulkWrite("users", operations,
+    mongoClient.bulkWrite("users", operations,
       arClient -> {
-        client.find("users", new JsonObject(), ar -> {
-          assertEquals(ar.result().size(), 2);
+        mongoClient.find("users", new JsonObject(), ar -> {
+          assertThat(ar.result()).hasSize(2);
           checks.flag();
         });
-        client.find("users", new JsonObject().put("lvl", 4), ar -> {
-          assertEquals(ar.result().size(), 1);
+        mongoClient.find("users", new JsonObject().put("lvl", 4), ar -> {
+          assertThat(ar.result()).hasSize(1);
           checks.flag();
         });
       });
