0aef069ab0bab0191bcfeba4a0a08e3ea0b13340
Oleg Tsvetkov <olecve@gmail.com>
2019-04-20 15:46:44 +0300

    Simplify the test to follow AAA pattern more clearly

--- server/src/test/java/org/obis/smalldata/dwca/EmlGeneratorTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/EmlGeneratorTest.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.Test;
+import org.obis.smalldata.dwca.EmlGenerator.DataSetEmlMap;
 import org.obis.smalldata.util.IoFile;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.diff.DefaultNodeMatcher;
+import org.xmlunit.diff.Diff;
 import org.xmlunit.diff.ElementSelectors;
 
 import java.io.IOException;
@@ -21,29 +23,32 @@ public class EmlGeneratorTest {
   private static ObjectMapper mapper = new ObjectMapper();
 
   @Test
-  void generateDataSetEmlMapsFromJson() throws IOException {
+  void generateDataSetEmlMapsFromJsonObject() throws IOException {
     var emlGenerator = new EmlGenerator();
     var datasets = IoFile.loadFromResources("testdata/dwca/datasets.json");
+    var json = mapper.<List<Map<String, Object>>>readValue(datasets, new TypeReference<List<Map<String, Object>>>() {
+    });
 
-    var json = mapper.<List<Map<String, Object>>>readValue(
-      datasets,
-      new TypeReference<List<Map<String, Object>>>() {
-      });
-
-    assertThat(json).hasSize(4);
-    json.stream()
+    var dataSetEmlMaps = json.stream()
       .map(JsonObject::new)
       .map(emlGenerator::generate)
-      .map(Optional::get)
-      .map(xml -> DiffBuilder
-        .compare(xml.getEml())
-        .withTest(IoFile.loadFromResources("testdata/dwca/" + xml.getId() + ".xml"))
-        .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byName))
-        .checkForSimilar()
-        .ignoreWhitespace()
-        .normalizeWhitespace()
-        .ignoreComments()
-        .build())
-      .forEach(diff -> assertThat(diff.hasDifferences()).isFalse());
+      .map(Optional::get);
+
+    assertThat(dataSetEmlMaps)
+      .hasSameSizeAs(json)
+      .hasSize(4)
+      .noneMatch(dataSetEmlMap -> diffWithExpectedXml(dataSetEmlMap).hasDifferences());
+  }
+
+  private static Diff diffWithExpectedXml(DataSetEmlMap dataSetEmlMap) {
+    return DiffBuilder
+      .compare(dataSetEmlMap.getEml())
+      .withTest(IoFile.loadFromResources("testdata/dwca/" + dataSetEmlMap.getId() + ".xml"))
+      .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byName))
+      .checkForSimilar()
+      .ignoreWhitespace()
+      .normalizeWhitespace()
+      .ignoreComments()
+      .build();
   }
 }
