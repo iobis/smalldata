ffcc3ef89f9ef6b0dbf8d4d97a56834c29d12679
Oleg Tsvetkov <olecve@gmail.com>
2019-04-29 12:23:36 +0300

    Improve test coverage

--- occurrences-app/src/components/pages/Occurrence/OccurrenceForm.spec.js
+++ occurrences-app/src/components/pages/Occurrence/OccurrenceForm.spec.js
@@ -6,5 +6,12 @@ describe('OccurrenceForm', () => {
   it('renders correctly', () => {
     const wrapper = mount(<OccurrenceForm/>)
     expect(wrapper).toMatchSnapshot()
+
+    wrapper.find('.step-3 .step-header').simulate('click')
+    expect(wrapper).toMatchSnapshot()
+
+    wrapper.find('.institution-code input').simulate('change', { target: { value: 'institution code' } })
+    wrapper.find('.step-4 .step-header').simulate('click')
+    expect(wrapper).toMatchSnapshot()
   })
 })
