40a13d446e35624860cf68a6f4a66fb4004f7483
kurt.sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-05-02 12:33:46 +0200

    WIP: clean up code (starter verticle)
    [ci skip]

--- server/src/main/java/org/obis/smalldata/Starter.java
+++ server/src/main/java/org/obis/smalldata/Starter.java
@@ -1,6 +1,5 @@
 package org.obis.smalldata;
 
-import com.google.common.base.Throwables;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
@@ -9,24 +8,18 @@ import org.obis.smalldata.auth.Auth;
 import org.obis.smalldata.dbcontroller.EmbeddedDb;
 import org.obis.smalldata.dwca.Dwca;
 import org.obis.smalldata.rss.RssComponent;
+import org.obis.smalldata.user.User;
 import org.obis.smalldata.webapi.WebApi;
 
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.ECGenParameterSpec;
-import java.util.Base64;
-
 import static org.pmw.tinylog.Logger.debug;
-import static org.pmw.tinylog.Logger.error;
 import static org.pmw.tinylog.Logger.info;
 
 public class Starter extends AbstractVerticle {
 
-  private static final String PUBLIC_KEY = "publicKey";
-  private static final String SECURITY_KEY = "securityKey";
+  private void deploy(Class verticleClass, String configKey) {
+    vertx.deployVerticle(verticleClass.getName(),
+      new DeploymentOptions().setConfig(config().getJsonObject(configKey)));
+  }
 
   @Override
   public void start(Future<Void> startFuture) {
@@ -43,46 +36,11 @@ public class Starter extends AbstractVerticle {
       new DeploymentOptions().setConfig(config().getJsonObject("storage")),
       deployHandler -> {
         info("Deployed Embedded DB verticle {}", deployHandler.result());
-        vertx.deployVerticle(Dwca.class.getName());
-        vertx.deployVerticle(RssComponent.class.getName());
-        vertx.deployVerticle(
-          WebApi.class.getName(),
-          new DeploymentOptions().setConfig(config().getJsonObject("http")));
-        try {
-          vertx.deployVerticle(
-            Auth.class.getName(),
-            new DeploymentOptions().setConfig(updateAuthConfig(config().getJsonObject("auth"))));
-        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException e) {
-          error(Throwables.getStackTraceAsString(e));
-          vertx.undeploy(this.deploymentID());
-        }
+        deploy(User.class, "user");
+        deploy(Dwca.class, "dwca");
+        deploy(RssComponent.class, "rss");
+        deploy(WebApi.class, "http");
+        deploy(Auth.class, "auth");
       });
   }
-
-  private static JsonObject updateAuthConfig(JsonObject authConfig)
-    throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
-    if ("local".equals(authConfig.getString("provider", "local"))
-      && isNullOrBlank(authConfig.getString(PUBLIC_KEY)) || isNullOrBlank(authConfig.getString(SECURITY_KEY))) {
-      KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
-      ECGenParameterSpec spec = new ECGenParameterSpec("secp256r1");
-      generator.initialize(spec);
-      KeyPair keyPair = generator.generateKeyPair();
-      authConfig.put(PUBLIC_KEY, createPublicKey(keyPair));
-      authConfig.put(SECURITY_KEY, createSecretKey(keyPair));
-    }
-    return authConfig;
-  }
-
-  private static String createPublicKey(KeyPair keyPair) {
-    return new String(Base64.getEncoder().encode(keyPair.getPublic().getEncoded()), StandardCharsets.UTF_8);
-  }
-
-  private static String createSecretKey(KeyPair keyPair) {
-    return new String(Base64.getEncoder().encode(keyPair.getPrivate().getEncoded()), StandardCharsets.UTF_8);
-  }
-
-  private static boolean isNullOrBlank(String val) {
-    return val == null || val.isBlank();
-  }
-
 }
--- server/src/main/java/org/obis/smalldata/auth/Auth.java
+++ server/src/main/java/org/obis/smalldata/auth/Auth.java
@@ -7,9 +7,18 @@ import io.vertx.ext.auth.PubSecKeyOptions;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.ECGenParameterSpec;
+import java.util.Base64;
 
+import static org.pmw.tinylog.Logger.error;
 import static org.pmw.tinylog.Logger.info;
+import static org.pmw.tinylog.Logger.warn;
 
 public class Auth extends AbstractVerticle {
 
@@ -18,31 +27,53 @@ public class Auth extends AbstractVerticle {
   private static final String ALG_KEY = "alg";
   private static final String AUTH_ES256 = "ES256";
 
-  @Override
-  public void start(Future<Void> startFuture) throws ExceptionInInitializerError {
-    info("starting module 'Auth'");
-    try {
-      var loginHandler = new LoginHandler(generateAuthProvider());
-      vertx.eventBus().localConsumer("auth.login", loginHandler::login);
-      startFuture.complete();
-    } catch (Exception e) {
-      startFuture.fail(new ExceptionInInitializerError("cannot deploy Auth module, invalid provider"));
-    }
+  private static String createPublicKey(KeyPair keyPair) {
+    return new String(Base64.getEncoder().encode(keyPair.getPublic().getEncoded()), StandardCharsets.UTF_8);
   }
 
-  private AuthProvider generateAuthProvider() throws InvalidKeyException {
+  private static String createSecretKey(KeyPair keyPair) {
+    return new String(Base64.getEncoder().encode(keyPair.getPrivate().getEncoded()), StandardCharsets.UTF_8);
+  }
+
+  private static boolean isNullOrBlank(String val) {
+    return val == null || val.isBlank();
+  }
+
+  @Override
+  public void start(Future<Void> startFuture) {
+    info("starting module 'Auth'");
+    LoginHandler loginHandler = null;
+    try {
+      loginHandler = new LoginHandler(generateAuthProvider());
+      startFuture.complete();
+    } catch (InvalidKeyException | InvalidAlgorithmParameterException | NoSuchAlgorithmException e) {
+      error("Cannot start Auth component: possibly wrong algorithm or keys for JWT signing/verification? {}",
+        e.getCause());
+      startFuture.fail(e);
+    }
+    vertx.eventBus().localConsumer("auth.login", loginHandler::login);
+  }
+
+  private AuthProvider generateAuthProvider() throws InvalidKeyException,
+    InvalidAlgorithmParameterException, NoSuchAlgorithmException {
     AuthProvider provider;
     if ("local".equals(config().getString("provider", "local"))) {
-      if (config().getString(PUBLIC_KEY).isBlank() || config().getString(SECURITY_KEY).isBlank()) {
-        throw new InvalidKeyException("Need to provide a public key for non-local JWT authorization");
-      } else {
-        provider = JWTAuth.create(vertx, new JWTAuthOptions()
-          .addPubSecKey(new PubSecKeyOptions()
-            .setAlgorithm(config().getString(ALG_KEY, AUTH_ES256))
-            .setPublicKey(config().getString(PUBLIC_KEY))
-            .setSecretKey(config().getString(SECURITY_KEY))
-          ));
+      if (isNullOrBlank(config().getString(PUBLIC_KEY))
+        || isNullOrBlank(config().getString(SECURITY_KEY))) {
+        warn("generating a new keypair for local JWT generation");
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
+        ECGenParameterSpec spec = new ECGenParameterSpec("secp256r1");
+        generator.initialize(spec);
+        KeyPair keyPair = generator.generateKeyPair();
+        config().put(PUBLIC_KEY, createPublicKey(keyPair));
+        config().put(SECURITY_KEY, createSecretKey(keyPair));
       }
+      provider = JWTAuth.create(vertx, new JWTAuthOptions()
+        .addPubSecKey(new PubSecKeyOptions()
+          .setAlgorithm(config().getString(ALG_KEY, AUTH_ES256))
+          .setPublicKey(config().getString(PUBLIC_KEY))
+          .setSecretKey(config().getString(SECURITY_KEY))
+        ));
     } else {
       if (config().getString(PUBLIC_KEY).isBlank()) {
         throw new InvalidKeyException("Need to provide a public key for non-local JWT authorization");
