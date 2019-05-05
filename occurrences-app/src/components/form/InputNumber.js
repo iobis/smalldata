99bf837de6a4c5e98f0a20791840431f9e1c51ac
Oleg Tsvetkov <olecve@gmail.com>
2019-05-05 09:39:19 +0300

    Add InputNumber component

---
+++ occurrences-app/src/components/form/InputNumber.js
@@ -0,0 +1,37 @@
+import classNames from 'classnames'
+import i18next from 'i18next'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+export default function InputNumber({ className, name, onChange, optional, value, step }) {
+  const { t } = useTranslation()
+  const label = t(name + '.label')
+  const placeholderKey = name + '.placeholder'
+  const helpKey = name + '.help'
+
+  return (
+    <div className={classNames('column field', className)}>
+      <label className={classNames('label', { 'has-text-weight-normal': optional })}>
+        {label}
+      </label>
+      <input
+        className="input"
+        onChange={(e) => onChange(parseFloat(e.target.value))}
+        placeholder={i18next.exists(placeholderKey) ? t(placeholderKey) : undefined}
+        type="number"
+        step={step}
+        value={value}/>
+      {i18next.exists(helpKey) ? <p className="help">{t(helpKey)}</p> : null}
+    </div>
+  )
+}
+
+InputNumber.propTypes = {
+  className: PropTypes.string,
+  name:      PropTypes.string.isRequired,
+  onChange:  PropTypes.func.isRequired,
+  optional:  PropTypes.bool,
+  step:      PropTypes.number,
+  value:     PropTypes.string
+}
