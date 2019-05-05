b0bd4c59ab6fd569b89d25205ff2c5781b0cfb25
Oleg Tsvetkov <olecve@gmail.com>
2019-05-05 19:23:25 +0300

    Improve test coverage of LocationData

--- occurrences-app/src/components/pages/Occurrence/LocationData/LocationData.spec.js
+++ occurrences-app/src/components/pages/Occurrence/LocationData/LocationData.spec.js
@@ -3,13 +3,47 @@ import React from 'react'
 import { getEmptyData } from './LocationData.fixture'
 import { mount } from 'enzyme'
 
-describe('SelectDataset', () => {
+describe('LocationData', () => {
   it('renders correctly', () => {
     expect(mount(createComponent())).toMatchSnapshot()
   })
+
+  it('returns updated data when changing decimal-longitude', () => {
+    const onChange = jest.fn()
+    const wrapper = mount(createComponent({ onChange }))
+    wrapper.find('.decimal-longitude input').simulate('change', { target: { value: '0.12345' } })
+    expect(onChange).toHaveBeenCalledTimes(1)
+    expect(onChange).toBeCalledWith({
+      coordinateUncertainty: null,
+      decimalLatitude:       null,
+      decimalLongitude:      0.12345,
+      maximumDepth:          null,
+      minimumDepth:          null,
+      verbatimCoordinates:   '',
+      verbatimDepth:         '',
+      verbatimEventDate:     ''
+    })
+  })
+
+  it('returns updated data when changing decimal-latitude', () => {
+    const onChange = jest.fn()
+    const wrapper = mount(createComponent({ onChange }))
+    wrapper.find('.decimal-longitude input').simulate('change', { target: { value: '-0.12345' } })
+    expect(onChange).toHaveBeenCalledTimes(1)
+    expect(onChange).toBeCalledWith({
+      coordinateUncertainty: null,
+      decimalLatitude:       null,
+      decimalLongitude:      -0.12345,
+      maximumDepth:          null,
+      minimumDepth:          null,
+      verbatimCoordinates:   '',
+      verbatimDepth:         '',
+      verbatimEventDate:     ''
+    })
+  })
 })
 
-function createComponent() {
+function createComponent(props) {
   const defaultProps = { data: getEmptyData(), onChange: () => {} }
-  return <LocationData {...defaultProps}/>
+  return <LocationData {...defaultProps} {...props}/>
 }
