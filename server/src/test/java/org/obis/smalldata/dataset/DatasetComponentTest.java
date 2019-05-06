491dedcd3b57a796528696a5e33104cc82fb7846
Oleg Tsvetkov <olecve@gmail.com>
2019-05-06 13:05:50 +0300

    Update test to use proper assertions

--- server/src/test/java/org/obis/smalldata/dataset/DatasetComponentTest.java
+++ server/src/test/java/org/obis/smalldata/dataset/DatasetComponentTest.java
@@ -23,14 +23,14 @@ import static org.pmw.tinylog.Logger.info;
 @ExtendWith(VertxExtension.class)
 public class DatasetComponentTest {
 
-  private TestDb testDb;
+  private static TestDb testDb;
 
   @BeforeEach
-  void deployVerticle(Vertx vertx, VertxTestContext testContext) {
+  void setUp(Vertx vertx, VertxTestContext testContext) {
     testDb = new TestDb();
     testDb.init(vertx);
-
-    vertx.sharedData().getLocalMap("settings")
+    vertx.sharedData()
+      .getLocalMap("settings")
       .put("storage", new JsonObject()
         .put("host", "localhost")
         .put("port", 12345)
@@ -41,44 +41,40 @@ public class DatasetComponentTest {
   }
 
   @AfterEach
-  public void stop() {
+  public void tearDown() {
     info("shutdown mongo db");
     testDb.shutDown();
   }
 
   @Test
-  @DisplayName("get all datasets")
   @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
-  void allDatasets(Vertx vertx, VertxTestContext testContext) {
-    vertx.eventBus().<JsonArray>send("datasets.query", new JsonObject(),
-      m -> {
-        var datasets = m.result().body();
-        assertThat(4)
-          .isEqualTo(datasets.size());
+  void getAllDataSets(Vertx vertx, VertxTestContext testContext) {
+    vertx.eventBus().<JsonArray>send(
+      "datasets.query",
+      new JsonObject(),
+      ar -> {
+        var datasets = ar.result().body();
+        assertThat(datasets).hasSize(4);
         var refs = datasets.stream()
           .map(JsonObject.class::cast)
-          .map(ds -> ds.getString("ref"))
+          .map(jsonObject -> jsonObject.getString("ref"))
           .collect(Collectors.toSet());
-        assertThat(Set.of("NnqVLwIyPn-nRkc", "wEaBfmFyQhYCdsk", "ntDOtUc7XsRrIus", "PoJnGNMaxsupE4w"))
-          .isEqualTo(refs);
+        assertThat(refs).containsExactlyInAnyOrder("NnqVLwIyPn-nRkc", "wEaBfmFyQhYCdsk", "ntDOtUc7XsRrIus", "PoJnGNMaxsupE4w");
         testContext.completeNow();
       });
   }
 
   @Test
-  @DisplayName("get one dataset")
   @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
-  void oneDataset(Vertx vertx, VertxTestContext testContext) {
+  void getOneDataset(Vertx vertx, VertxTestContext testContext) {
     var ref = "ntDOtUc7XsRrIus";
-    vertx.eventBus().<JsonArray>send("datasets.query",
+    vertx.eventBus().<JsonArray>send(
+      "datasets.query",
       new JsonObject().put("ref", ref),
-      m -> {
-        var datasets = m.result().body();
-        info(datasets);
-        assertThat(1)
-          .isEqualTo(datasets.size());
-        assertThat(datasets.getJsonObject(0).getString("ref"))
-          .isEqualTo(ref);
+      ar -> {
+        var datasets = ar.result().body();
+        assertThat(datasets).hasSize(1);
+        assertThat(datasets.getJsonObject(0).getString("ref")).isEqualTo(ref);
         testContext.completeNow();
       });
   }
