655f4fc6127bbc135bf81e57ce4772a3038d8d65
Oleg Tsvetkov <olecve@gmail.com>
2019-05-06 14:47:38 +0300

    Fix DbQueryTest
    as was failing completely on windows

--- server/src/test/java/org/obis/smalldata/dwca/DbQueryTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/DbQueryTest.java
@@ -1,7 +1,6 @@
 package org.obis.smalldata.dwca;
 
 import io.vertx.core.CompositeFuture;
-import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.ext.mongo.MongoClient;
 import org.junit.jupiter.api.AfterEach;
@@ -12,31 +11,24 @@ import org.obis.smalldata.testutil.TestDb;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import static org.pmw.tinylog.Logger.info;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DbQueryTest {
   private TestDb testDb;
   private MongoClient mongoClient;
   private DbQuery dbQuery;
-  private Future<Boolean> waitForResult;
 
   @BeforeEach
-  public void start() {
-    waitForResult = Future.future();
-    waitForResult.setHandler(done -> {
-      info("shutdown mongo db");
-      mongoClient.close();
-      testDb.shutDown();
-    });
+  public void setUp() {
     testDb = new TestDb();
     mongoClient = testDb.init(Vertx.vertx());
-    info(mongoClient);
     dbQuery = new DbQuery(mongoClient);
   }
 
   @AfterEach
-  public void stop() {
-    info("-- Test done.");
+  public void tearDown() {
+    mongoClient.close();
+    testDb.shutDown();
   }
 
   @Test
@@ -45,15 +37,13 @@ class DbQueryTest {
     var dwcaRecords = dbQuery.dwcaRecords(datasetRef);
     var dataset = dbQuery.dataset(datasetRef);
     var countDownLatch = new CountDownLatch(1);
-    CompositeFuture.all(dataset, dwcaRecords).setHandler(res -> {
-      info(res.result().list().get(0));
-      info(res.result().list().get(1));
-      waitForResult.complete();
+    CompositeFuture.all(dataset, dwcaRecords).setHandler(ar -> {
+      var result = ar.result();
+      assertThat(result.list()).hasSize(2);
       countDownLatch.countDown();
     });
     if (!countDownLatch.await(1000, TimeUnit.MILLISECONDS)) {
       throw new InterruptedException("Couldn't write data to database");
     }
   }
-
 }
