a444c3a42fdc05ab373129e9bf952166c25a168a
Oleg Tsvetkov <olecve@gmail.com>
2019-05-06 14:52:29 +0300

    Rewrite DbQueryTest to use Vertx test extension and to test each method in isolation

--- server/src/test/java/org/obis/smalldata/dwca/DbQueryTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/DbQueryTest.java
@@ -1,18 +1,18 @@
 package org.obis.smalldata.dwca;
 
-import io.vertx.core.CompositeFuture;
 import io.vertx.core.Vertx;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.obis.smalldata.testutil.TestDb;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(VertxExtension.class)
 class DbQueryTest {
   private TestDb testDb;
   private MongoClient mongoClient;
@@ -32,18 +32,24 @@ class DbQueryTest {
   }
 
   @Test
-  void testDatasetFuture() throws InterruptedException {
+  void dwcaRecords(VertxTestContext testContext) {
     var datasetRef = "NnqVLwIyPn-nRkc";
     var dwcaRecords = dbQuery.dwcaRecords(datasetRef);
-    var dataset = dbQuery.dataset(datasetRef);
-    var countDownLatch = new CountDownLatch(1);
-    CompositeFuture.all(dataset, dwcaRecords).setHandler(ar -> {
+    dwcaRecords.setHandler(ar -> {
       var result = ar.result();
-      assertThat(result.list()).hasSize(2);
-      countDownLatch.countDown();
+      assertThat(result).hasSize(642);
+      testContext.completeNow();
     });
-    if (!countDownLatch.await(1000, TimeUnit.MILLISECONDS)) {
-      throw new InterruptedException("Couldn't write data to database");
-    }
+  }
+
+  @Test
+  void dataset(VertxTestContext testContext) {
+    var datasetRef = "NnqVLwIyPn-nRkc";
+    var dataset = dbQuery.dataset(datasetRef);
+    dataset.setHandler(ar -> {
+      var result = ar.result();
+      assertThat(result).hasSize(12);
+      testContext.completeNow();
+    });
   }
 }
