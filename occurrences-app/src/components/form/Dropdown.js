e3ed8256f6ccf30d5c8fcfdd72b31ee8178ccfce
Oleg Tsvetkov <olecve@gmail.com>
2019-05-07 20:23:51 +0300

    Extract dropdown to separate component

---
+++ occurrences-app/src/components/form/Dropdown.js
@@ -0,0 +1,52 @@
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useOnClickOutside } from '../../hooks/hooks'
+import React, { useRef, useState } from 'react'
+
+export default function Dropdown({ onChange, options, value }) {
+  const ref = useRef()
+  const [active, setActive] = useState(false)
+  const [selectedValue, setSelectedValue] = useState(value)
+  const hideOptions = () => setActive(false)
+  const showOptions = () => {if (active === false) setActive(true)}
+  const handleChange = (item) => {
+    onChange(item)
+    setSelectedValue(item)
+    setActive(false)
+  }
+
+  useOnClickOutside(ref, hideOptions)
+
+  return (
+    <div className={classNames('dropdown', { 'is-active': active })} onClick={showOptions} ref={ref}>
+      <div className="dropdown-trigger">
+        <button aria-controls="dropdown-menu" aria-haspopup="true" className="button">
+          <span>{selectedValue}</span>
+          <span className="icon is-small">
+            <FontAwesomeIcon className="angle-down" icon="angle-down"/>
+          </span>
+        </button>
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {options.map(option => (
+            <a
+              className={classNames('dropdown-item', { 'is-active': option === selectedValue })}
+              href="#"
+              key={option}
+              onClick={() => handleChange(option)}>
+              {option}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Dropdown.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  options:  PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  value:    PropTypes.string.isRequired
+}
--- occurrences-app/src/components/pages/Occurrence/MeasurementOrFact/MeasurementOrFact.js
+++ occurrences-app/src/components/pages/Occurrence/MeasurementOrFact/MeasurementOrFact.js
@@ -1,10 +1,8 @@
-import classNames from 'classnames'
 import CopyPreviousData from '../CopyPreviousData'
+import Dropdown from '../../../form/Dropdown'
 import PropTypes from 'prop-types'
-import React, { useRef, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
 import { getGeneralMeasurements, getSpecificMeasurements } from '../../../../clients/measurments'
-import { useOnClickOutside } from '../../../../hooks/hooks'
 import { useTranslation } from 'react-i18next'
 
 export default function MeasurementOrFact({ onChange }) {
@@ -137,51 +135,4 @@ MeasurementRow.propTypes = {
   units:      PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired
   })).isRequired
-}
-
-function Dropdown({ value, options, onChange }) {
-  const ref = useRef()
-  const [active, setActive] = useState(false)
-  const [selectedValue, setSelectedValue] = useState(value)
-  const hideOptions = () => setActive(false)
-  const showOptions = () => {if (active === false) setActive(true)}
-  const handleChange = (item) => {
-    onChange(item)
-    setSelectedValue(item)
-    setActive(false)
-  }
-
-  useOnClickOutside(ref, hideOptions)
-
-  return (
-    <div className={classNames('dropdown', { 'is-active': active })} onClick={showOptions} ref={ref}>
-      <div className="dropdown-trigger">
-        <button aria-controls="dropdown-menu" aria-haspopup="true" className="button">
-          <span>{selectedValue}</span>
-          <span className="icon is-small">
-            <FontAwesomeIcon className="angle-down" icon="angle-down"/>
-          </span>
-        </button>
-      </div>
-      <div className="dropdown-menu" id="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          {options.map(option => (
-            <a
-              className={classNames('dropdown-item', { 'is-active': option === selectedValue })}
-              href="#"
-              key={option}
-              onClick={() => handleChange(option)}>
-              {option}
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
-
-Dropdown.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  options:  PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  value:    PropTypes.string.isRequired
 }
