9562122c8c49b781da11a5dff73c1f97249aaa44
Oleg Tsvetkov <olecve@gmail.com>
2019-05-07 20:16:06 +0300

    Pass added data to supplied measurements array

--- occurrences-app/src/components/pages/Occurrence/MeasurementOrFact/MeasurementOrFact.js
+++ occurrences-app/src/components/pages/Occurrence/MeasurementOrFact/MeasurementOrFact.js
@@ -1,22 +1,23 @@
 import classNames from 'classnames'
-import { getGeneralMeasurements, getSpecificMeasurements } from '../../../../clients/measurments'
 import CopyPreviousData from '../CopyPreviousData'
 import PropTypes from 'prop-types'
 import React, { useRef, useState } from 'react'
-import { useTranslation } from 'react-i18next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { getGeneralMeasurements, getSpecificMeasurements } from '../../../../clients/measurments'
 import { useOnClickOutside } from '../../../../hooks/hooks'
+import { useTranslation } from 'react-i18next'
 
-export default function MeasurementOrFact({ data, onChange }) {
+export default function MeasurementOrFact({ onChange }) {
   const { t } = useTranslation()
   const generalMeasurements = getGeneralMeasurements()
   const specificMeasurements = getSpecificMeasurements()
-  const suppliedMeasurements = [{
-    type:   'Temperature',
-    typeId: 'http://vocab.nerc.ac.uk/collection/P01/current/TEMPCU01/',
-    units:  [{ name: 'Degrees Celsius', id: 'http://vocab.nerc.ac.uk/collection/P06/current/UPAA' }],
-    value:  '10'
-  }]
+  const [suppliedMeasurements, setSuppliedMeasurements] = useState([])
+
+  function addSuppliedMeasurements(measurment) {
+    const updatedMeasurements = [...suppliedMeasurements, measurment]
+      .sort((left, right) => left.type.toLowerCase().localeCompare(right.type.toLowerCase()))
+    setSuppliedMeasurements(updatedMeasurements)
+  }
 
   return (
     <div className="measurement-or-fact section is-fluid">
@@ -34,29 +35,17 @@ export default function MeasurementOrFact({ data, onChange }) {
             </tr>
             </thead>
             <tbody>
-            {generalMeasurements.map(generalMeasurement =>
-              <tr className="fieldrow" key={generalMeasurement.type}>
-                <td>{generalMeasurement.type}</td>
-                <td>
-                  <Dropdown
-                    onChange={(value) => console.log(value)}
-                    options={generalMeasurement.units.map(unit => unit.name)}
-                    value={generalMeasurement.units[0].name}/>
-                </td>
-                <td>
-                  <input
-                    className="input"
-                    onChange={(e) => onChange(e.target.value)}
-                    type="text"
-                    value=""/>
-                </td>
-                <td><a className="button" onClick={() => {}}>add</a></td>
-              </tr>
-            )}
+            {generalMeasurements.map(generalMeasurement => (
+              <MeasurementRow
+                key={generalMeasurement.type}
+                onClickAdd={addSuppliedMeasurements}
+                type={generalMeasurement.type}
+                units={generalMeasurement.units}/>
+            ))}
             </tbody>
           </table>
           <h1 className="title">{t('occurrenceForm.measurementOrFact.specific.title')}</h1>
-          <h2 className='subtitle'>{t('occurrenceForm.measurementOrFact.specific.subtitle')}</h2>
+          <h2 className="subtitle">{t('occurrenceForm.measurementOrFact.specific.subtitle')}</h2>
           <table className="table is-fullwidth is-striped is-hoverable">
             <thead>
             <tr>
@@ -67,25 +56,13 @@ export default function MeasurementOrFact({ data, onChange }) {
             </tr>
             </thead>
             <tbody>
-            {specificMeasurements.map(specificMeasurement =>
-              <tr className="fieldrow" key={specificMeasurement.type}>
-                <td>{specificMeasurement.type}</td>
-                <td>
-                  <Dropdown
-                    onChange={(value) => console.log(value)}
-                    options={specificMeasurement.units.map(unit => unit.name)}
-                    value={specificMeasurement.units[0].name}/>
-                </td>
-                <td>
-                  <input
-                    className="input"
-                    onChange={(e) => onChange(e.target.value)}
-                    type="text"
-                    value=""/>
-                </td>
-                <td><a className="button" onClick={() => {}}>add</a></td>
-              </tr>
-            )}
+            {specificMeasurements.map(specificMeasurement => (
+              <MeasurementRow
+                key={specificMeasurement.type}
+                onClickAdd={addSuppliedMeasurements}
+                type={specificMeasurement.type}
+                units={specificMeasurement.units}/>
+            ))}
             </tbody>
           </table>
         </div>
@@ -102,25 +79,14 @@ export default function MeasurementOrFact({ data, onChange }) {
             </tr>
             </thead>
             <tbody>
-            {suppliedMeasurements.map(suppliedMeasurements =>
-              <tr className="fieldrow" key={suppliedMeasurements.type}>
-                <td>{suppliedMeasurements.type}</td>
-                <td>
-                  <Dropdown
-                    onChange={(value) => console.log(value)}
-                    options={suppliedMeasurements.units.map(unit => unit.name)}
-                    value={suppliedMeasurements.units[0].name}/>
-                </td>
-                <td>
-                  <input
-                    className="input"
-                    onChange={(e) => onChange(e.target.value)}
-                    type="text"
-                    value={suppliedMeasurements.value}/>
-                </td>
+            {suppliedMeasurements.map(({ type, unit, value }) => (
+              <tr className="fieldrow" key={type + unit + value}>
+                <td>{type}</td>
+                <td>{unit}</td>
+                <td>{value}</td>
                 <td><a className="button" onClick={() => {}}>remove</a></td>
               </tr>
-            )}
+            ))}
             </tbody>
           </table>
         </div>
@@ -132,6 +98,45 @@ export default function MeasurementOrFact({ data, onChange }) {
 
 MeasurementOrFact.propTypes = {
   onChange: PropTypes.func.isRequired
+}
+
+function MeasurementRow({ onClickAdd, type, units }) {
+  const [selectedUnit, setSelectedUnit] = useState(units[0])
+  const [selectedValue, setSelectedValue] = useState('')
+
+  return (
+    <tr className="fieldrow">
+      <td>{type}</td>
+      <td>
+        <Dropdown
+          onChange={(value) => setSelectedUnit(value)}
+          options={units.map(unit => unit.name)}
+          value={selectedUnit.name}/>
+      </td>
+      <td>
+        <input
+          className="input"
+          onChange={(e) => setSelectedValue(e.target.value)}
+          type="text"
+          value={selectedValue}/>
+      </td>
+      <td>
+        <a
+          className="button"
+          onClick={() => onClickAdd({ unit: selectedUnit.name, type, value: selectedValue })}>
+          add
+        </a>
+      </td>
+    </tr>
+  )
+}
+
+MeasurementRow.propTypes = {
+  onClickAdd: PropTypes.func.isRequired,
+  type:       PropTypes.string.isRequired,
+  units:      PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired
+  })).isRequired
 }
 
 function Dropdown({ value, options, onChange }) {
@@ -151,7 +156,7 @@ function Dropdown({ value, options, onChange }) {
   return (
     <div className={classNames('dropdown', { 'is-active': active })} onClick={showOptions} ref={ref}>
       <div className="dropdown-trigger">
-        <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+        <button aria-controls="dropdown-menu" aria-haspopup="true" className="button">
           <span>{selectedValue}</span>
           <span className="icon is-small">
             <FontAwesomeIcon className="angle-down" icon="angle-down"/>
@@ -163,8 +168,8 @@ function Dropdown({ value, options, onChange }) {
           {options.map(option => (
             <a
               className={classNames('dropdown-item', { 'is-active': option === selectedValue })}
-              key={option}
               href="#"
+              key={option}
               onClick={() => handleChange(option)}>
               {option}
             </a>
@@ -173,4 +178,10 @@ function Dropdown({ value, options, onChange }) {
       </div>
     </div>
   )
+}
+
+Dropdown.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  options:  PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  value:    PropTypes.string.isRequired
 }
