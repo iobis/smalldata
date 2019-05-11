5f3ca409c039d8c39e604b7f25f8e4b652cb7e83
Oleg Tsvetkov <olecve@gmail.com>
2019-05-11 12:03:34 +0300

    Improve test coverage of LocationPicker

--- occurrences-app/src/components/pages/Occurrence/LocationData/LocationPicker.spec.js
+++ occurrences-app/src/components/pages/Occurrence/LocationData/LocationPicker.spec.js
@@ -34,7 +34,7 @@ describe('LocationPicker', () => {
             'boundingbox':  ['59.9402802', '59.9403802', '30.3189035', '30.3190035'],
             'lat':          '59.9403302',
             'lon':          '30.3189535',
-            'display_name': 'Ля Русс, 37, Певческий проезд, Palace District, Центральный район, Saint Petersburg, Northwestern Federal District, 190000, Russia',
+            'display_name': 'Saint Petersburg, Northwestern Federal District, 190000, Russia',
             'class':        'amenity',
             'type':         'restaurant',
             'importance':   0.30100000000000005,
@@ -55,21 +55,45 @@ describe('LocationPicker', () => {
   })
 
   it('makes fetch request when changing input field', async() => {
+    const onChange = jest.fn()
     act(() => {
-      ReactDOM.render(createComponent(), container)
+      ReactDOM.render(createComponent({ onChange }), container)
     })
     expect(container.querySelectorAll('.suggestions-result .suggestion-row')).toHaveLength(0)
+    expect(container.querySelector('.search-string.input').value).toBe('')
 
     act(() => {
       TestUtils.Simulate.change(container.querySelector('.search-string'), { target: { value: 'St. Petersburg, Russ' } })
     })
     await waitUntil(() => fetch.mock.calls.length === 1)
+    expect(onChange).toHaveBeenCalledTimes(0)
     expect(fetch).toHaveBeenCalledTimes(1)
     expect(fetch).toHaveBeenNthCalledWith(1, 'https://nominatim.openstreetmap.org/search?format=json&q=St. Petersburg, Russ')
     expect(container.querySelectorAll('.suggestions-result .suggestion-row')).toHaveLength(1)
+    expect(container.querySelector('.search-string.input').value).toBe('St. Petersburg, Russ')
+
+    act(() => {
+      TestUtils.Simulate.click(container.querySelectorAll('.suggestions-result .suggestion-row')[0])
+    })
+    expect(onChange).toHaveBeenCalledTimes(1)
+    expect(onChange).toHaveBeenNthCalledWith(1, { latitude: '59.9403302', longitude: '30.3189535' })
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(container.querySelectorAll('.suggestions-result .suggestion-row')).toHaveLength(1)
+    expect(container.querySelector('.search-string.input').value).toBe('St. Petersburg, Russ')
+
+    act(() => {
+      TestUtils.Simulate.click(container.querySelector('.times-circle'))
+    })
+    expect(onChange).toHaveBeenCalledTimes(1)
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(container.querySelectorAll('.suggestions-result .suggestion-row')).toHaveLength(0)
+    expect(container.querySelector('.search-string.input').value).toBe('')
   })
 })
 
-function createComponent() {
-  return <LocationPicker onChange={jest.fn()}/>
+function createComponent(props) {
+  const defaultProps = {
+    onChange: jest.fn()
+  }
+  return <LocationPicker {...defaultProps} {...props}/>
 }
