51190bdfd5965bf0acbba054d6fc0de564e8debb
Oleg Tsvetkov <olecve@gmail.com>
2019-05-13 19:57:14 +0300

    Recover MeasurementOrFact unit test

--- occurrences-app/src/components/pages/Occurrence/MeasurementOrFact/MeasurementOrFact.spec.js
+++ occurrences-app/src/components/pages/Occurrence/MeasurementOrFact/MeasurementOrFact.spec.js
@@ -3,6 +3,28 @@ import React from 'react'
 import { getEmptyData } from './MeasurementOrFact.fixture'
 import { mount } from 'enzyme'
 
+jest.mock('../../../../clients/measurments', () => ({
+  getGeneralMeasurements:  () => (
+    [{
+      type:   'general-measurement-1',
+      typeId: 'http://general-measurement-1/',
+      units:  [
+        { name: 'general-measurement-1-unit-1', id: 'http://general-measurement-1-unit-1/' },
+        { name: 'general-measurement-1-unit-2', id: 'http://general-measurement-1-unit-2/' }
+      ]
+    }, {
+      type:   'general-measurement-2',
+      typeId: 'http://general-measurement-2/',
+      units:  [{ name: 'general-measurement-2-unit-1', id: 'general-measurement-2-unit-1' }]
+    }, {
+      type:   'general-measurement-3',
+      typeId: 'http://general-measurement-3/',
+      units:  [{ name: 'general-measurement-3-unit-1', id: 'general-measurement-3-unit-1' }]
+    }]
+  ),
+  getSpecificMeasurements: () => []
+}))
+
 describe('MeasurementOrFact', () => {
   const onChange = jest.fn()
   let wrapper
@@ -31,15 +53,18 @@ describe('MeasurementOrFact', () => {
 
       it('invokes onChange handler', () => {
         expect(onChange).toHaveBeenCalledTimes(1)
-    //    expect(onChange).toBeCalledWith([
-    //      { type: 'Pressure', unit: 'Kilogram', value: '10', units: getPressureUnits() }
-    //    ])
+        expect(onChange).toBeCalledWith([{
+          type:  'general-measurement-1',
+          unit:  'general-measurement-1-unit-1',
+          units: getPressureUnits(),
+          value: '10'
+        }])
       })
 
       it('renders supplied value with default unit', () => {
         expect(wrapper.find('.supplied .fieldrow')).toHaveLength(1)
-        expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('Pressure')
-    //    expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('Kilogram')
+        expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('general-measurement-1')
+        expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('general-measurement-1-unit-1')
         expect(wrapper.find('.supplied .fieldrow .input').instance().value).toBe('10')
       })
 
@@ -50,35 +75,41 @@ describe('MeasurementOrFact', () => {
 
         it('invokes onChange with update value', () => {
           expect(onChange).toHaveBeenCalledTimes(2)
-    //      expect(onChange).toHaveBeenNthCalledWith(2, [
-    //        { type: 'Pressure', unit: 'Kilogram', value: '125', units: getPressureUnits() }
-    //      ])
+          expect(onChange).toHaveBeenNthCalledWith(2, [{
+            type:  'general-measurement-1',
+            unit:  'general-measurement-1-unit-1',
+            units: getPressureUnits(),
+            value: '125'
+          }])
         })
 
         it('renders supplied value with updated value', () => {
           expect(wrapper.find('.supplied .fieldrow')).toHaveLength(1)
-          expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('Pressure')
-    //      expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('Kilogram')
+          expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('general-measurement-1')
+          expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('general-measurement-1-unit-1')
           expect(wrapper.find('.supplied .fieldrow .input').instance().value).toBe('125')
         })
 
         describe('and then updating unit', () => {
           beforeAll(() => {
             wrapper.find('.supplied .fieldrow .dropdown').at(0).simulate('click')
-    //        wrapper.find('.supplied .fieldrow .dropdown-item').at(1).simulate('click')
+            wrapper.find('.supplied .fieldrow .dropdown-item').at(1).simulate('click')
           })
 
           it('invokes onChange handler with updated unit', () => {
-    //        expect(onChange).toHaveBeenCalledTimes(3)
-    //        expect(onChange).toHaveBeenNthCalledWith(3, [
-    //          { type: 'Pressure', unit: 'Gram', value: '125', units: getPressureUnits() }
-    //        ])
+            expect(onChange).toHaveBeenCalledTimes(3)
+            expect(onChange).toHaveBeenNthCalledWith(3, [{
+              type:  'general-measurement-1',
+              unit:  'general-measurement-1-unit-2',
+              value: '125',
+              units: getPressureUnits()
+            }])
           })
 
           it('renders supplied with updated unit', () => {
             expect(wrapper.find('.supplied .fieldrow')).toHaveLength(1)
-            expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('Pressure')
-   //         expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('Gram')
+            expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('general-measurement-1')
+            expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('general-measurement-1-unit-2')
             expect(wrapper.find('.supplied .fieldrow .input').instance().value).toBe('125')
           })
 
@@ -88,20 +119,27 @@ describe('MeasurementOrFact', () => {
             })
 
             it('invokes onChange handler with copied measurement', () => {
-    //          expect(onChange).toHaveBeenCalledTimes(4)
-    //          expect(onChange).toHaveBeenNthCalledWith(4, [
-    //            { type: 'Pressure', unit: 'Gram', value: '125', units: getPressureUnits() },
-    //            { type: 'Pressure', unit: 'Gram', value: '125', units: getPressureUnits() }
-    //           ])
+              expect(onChange).toHaveBeenCalledTimes(4)
+              expect(onChange).toHaveBeenNthCalledWith(4, [{
+                type:  'general-measurement-1',
+                unit:  'general-measurement-1-unit-2',
+                value: '125',
+                units: getPressureUnits()
+              }, {
+                type:  'general-measurement-1',
+                unit:  'general-measurement-1-unit-2',
+                value: '125',
+                units: getPressureUnits()
+              }])
             })
 
             it('renders 2 identical supplied measurements', () => {
               expect(wrapper.find('.supplied .fieldrow')).toHaveLength(2)
-              expect(wrapper.find('.supplied .fieldrow .type').at(0).text()).toBe('Pressure')
-    //          expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').at(0).text()).toBe('Gram')
+              expect(wrapper.find('.supplied .fieldrow .type').at(0).text()).toBe('general-measurement-1')
+              expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').at(0).text()).toBe('general-measurement-1-unit-2')
               expect(wrapper.find('.supplied .fieldrow .input').at(0).instance().value).toBe('125')
-              expect(wrapper.find('.supplied .fieldrow .type').at(1).text()).toBe('Pressure')
-    //          expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').at(1).text()).toBe('Gram')
+              expect(wrapper.find('.supplied .fieldrow .type').at(1).text()).toBe('general-measurement-1')
+              expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').at(1).text()).toBe('general-measurement-1-unit-2')
               expect(wrapper.find('.supplied .fieldrow .input').at(1).instance().value).toBe('125')
             })
           })
@@ -132,15 +170,18 @@ describe('MeasurementOrFact', () => {
 
       it('invokes onChange handler', () => {
         expect(onChange).toHaveBeenCalledTimes(1)
-     //   expect(onChange).toBeCalledWith([
-     //     { type: 'Pressure', unit: 'Gram', value: '20', units: getPressureUnits() }
-     //   ])
+        expect(onChange).toBeCalledWith([{
+          type:  'general-measurement-1',
+          unit:  'general-measurement-1-unit-2',
+          value: '20',
+          units: getPressureUnits()
+        }])
       })
 
       it('renders supplied value with selected unit', () => {
         expect(wrapper.find('.supplied .fieldrow')).toHaveLength(1)
-        expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('Pressure')
-    //    expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('Gram')
+        expect(wrapper.find('.supplied .fieldrow .type').text()).toBe('general-measurement-1')
+        expect(wrapper.find('.supplied .fieldrow .dropdown .selected-value').text()).toBe('general-measurement-1-unit-2')
         expect(wrapper.find('.supplied .fieldrow .input').instance().value).toBe('20')
       })
     })
@@ -160,11 +201,11 @@ describe('MeasurementOrFact', () => {
 
     it('invokes onChange handler', () => {
       expect(onChange).toHaveBeenCalledTimes(3)
-     // expect(onChange).toHaveBeenNthCalledWith(3, [
-     //   { type: 'Pressure', unit: 'Kilogram', value: '10', units: getPressureUnits() },
-     //   { type: 'Pressure', unit: 'Kilogram', value: '20', units: getPressureUnits() },
-     //   { type: 'Pressure', unit: 'Kilogram', value: '30', units: getPressureUnits() }
-     // ])
+      expect(onChange).toHaveBeenNthCalledWith(3, [
+        { type: 'general-measurement-1', unit: 'general-measurement-1-unit-1', value: '10', units: getPressureUnits() },
+        { type: 'general-measurement-1', unit: 'general-measurement-1-unit-1', value: '20', units: getPressureUnits() },
+        { type: 'general-measurement-1', unit: 'general-measurement-1-unit-1', value: '30', units: getPressureUnits() }
+      ])
     })
 
     it('renders 3 measurements', () => {
@@ -179,10 +220,17 @@ describe('MeasurementOrFact', () => {
 
       it('invokes onChange handler', () => {
         expect(onChange).toHaveBeenCalledTimes(4)
-       // expect(onChange).toHaveBeenNthCalledWith(4, [
-       //   { type: 'Pressure', unit: 'Kilogram', value: '20', units: getPressureUnits() },
-       //   { type: 'Pressure', unit: 'Kilogram', value: '30', units: getPressureUnits() }
-       // ])
+        expect(onChange).toHaveBeenNthCalledWith(4, [{
+          type:  'general-measurement-1',
+          unit:  'general-measurement-1-unit-1',
+          value: '20',
+          units: getPressureUnits()
+        }, {
+          type:  'general-measurement-1',
+          unit:  'general-measurement-1-unit-1',
+          value: '30',
+          units: getPressureUnits()
+        }])
       })
 
       it('renders 2 measurements', () => {
@@ -197,9 +245,9 @@ describe('MeasurementOrFact', () => {
 
         it('invokes onChange handler', () => {
           expect(onChange).toHaveBeenCalledTimes(5)
-         // expect(onChange).toHaveBeenNthCalledWith(5, [
-         //   { type: 'Pressure', unit: 'Kilogram', value: '20', units: getPressureUnits() }
-         // ])
+          expect(onChange).toHaveBeenNthCalledWith(5, [
+            { type: 'general-measurement-1', unit: 'general-measurement-1-unit-1', value: '20', units: getPressureUnits() }
+          ])
         })
 
         it('renders 1 measurements', () => {
@@ -216,13 +264,10 @@ describe('MeasurementOrFact', () => {
   }
 
   function getPressureUnits() {
-    return [{
-      id:   'http://vocab.nerc.ac.uk/collection/P06/current/KGXX',
-      name: 'Kilogram'
-    }, {
-      id:   'http://vocab.nerc.ac.uk/collection/P06/current/UGRM/',
-      name: 'Gram'
-    }]
+    return [
+      { id: 'http://general-measurement-1-unit-1/', name: 'general-measurement-1-unit-1' },
+      { id: 'http://general-measurement-1-unit-2/', name: 'general-measurement-1-unit-2' }
+    ]
   }
 })
 
