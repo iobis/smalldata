acdd89ca332eb316e6b934f2c0d576a57559258a
Oleg Tsvetkov <olecve@gmail.com>
2019-05-15 21:46:53 +0300

    Don't pass anything to createContext

--- occurrences-app/src/context/AuthContext.js
+++ occurrences-app/src/context/AuthContext.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { createContext, useState } from 'react'
 
-export const AuthContext = createContext([{}, () => {}])
+export const AuthContext = createContext(null)
 
 export function AuthProvider({ children }) {
   const [loggedIn, setLoggedIn] = useState(false)
