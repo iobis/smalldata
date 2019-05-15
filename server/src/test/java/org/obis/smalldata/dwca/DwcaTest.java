427211002a0967feae18a8572c7f86a467a06636
Kurt Sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-05-15 22:57:19 +0200

    Revert "Merge pull request #159 from iobis/115.implement-GET-/users/{user-ref}/dwca/…-endpoint"
    This reverts commit 565d13ae5cd7f3a745d52269dcc9e8f3e310789d, reversing
    changes made to 7b1c9f0864dbf45c75d69006a489351cf815ac7a.

--- server/src/test/java/org/obis/smalldata/dwca/DwcaTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/DwcaTest.java
@@ -1,7 +1,6 @@
 package org.obis.smalldata.dwca;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
@@ -16,7 +15,6 @@ import org.obis.smalldata.testutil.TestDb;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.pmw.tinylog.Logger.info;
 
 @ExtendWith(VertxExtension.class)
@@ -28,13 +26,11 @@ public class DwcaTest {
   public static void deployVerticle(Vertx vertx, VertxTestContext testContext) {
     testDb = new TestDb();
     testDb.init(vertx);
-    var storageSetting = new JsonObject()
-      .put("host", "localhost")
-      .put("port", 12345)
-      .put("path", "");
-    vertx.sharedData().getLocalMap("settings").putAll(
-      Map.of(
-        "storage", storageSetting,
+    vertx.sharedData().getLocalMap("settings")
+      .putAll(Map.of("storage", new JsonObject()
+        .put("host", "localhost")
+          .put("port", 12345)
+          .put("path", ""),
         "baseUrl", "https://my.domain.org/"));
     vertx.deployVerticle(
       Dwca.class.getName(),
@@ -59,52 +55,6 @@ public class DwcaTest {
         if (ar.succeeded()) {
           JsonObject body = ar.result().body();
           info("success {}", body);
-          testContext.completeNow();
-        } else {
-          info("error {}", ar.cause());
-          testContext.failNow(ar.cause());
-        }
-      });
-  }
-
-  @Test
-  @DisplayName("get records for user")
-  @Timeout(value = 5, timeUnit = TimeUnit.SECONDS)
-  void testRecordsForUser(Vertx vertx, VertxTestContext testContext) {
-    vertx.eventBus().<JsonArray>send(
-      "dwca",
-      new JsonObject()
-        .put("action", "recordsForUser")
-        .put("userRef", "FsfEMwhUTO_8I68"),
-      ar -> {
-        if (ar.succeeded()) {
-          JsonArray body = ar.result().body();
-          info("success {}", body);
-          assertThat(body.stream().map(JsonObject.class::cast).anyMatch(rec -> rec.containsKey("_id"))).isFalse();
-          assertThat(body.size()).isEqualTo(2310);
-          testContext.completeNow();
-        } else {
-          info("error {}", ar.cause());
-          testContext.failNow(ar.cause());
-        }
-      });
-  }
-
-  @Test
-  @DisplayName("get one dwca record for a user")
-  @Timeout(value = 5, timeUnit = TimeUnit.SECONDS)
-  void testRecordForUser(Vertx vertx, VertxTestContext testContext) {
-    vertx.eventBus().<JsonObject>send(
-      "dwca",
-      new JsonObject()
-        .put("action", "recordForUser")
-        .put("userRef", "FsfEMwhUTO_8I68")
-        .put("dwcaId", "IBSS_R/V N. Danilevskiy 1935 Azov Sea benthos data_338"),
-      ar -> {
-        if (ar.succeeded()) {
-          var body = ar.result().body();
-          info("success {}", body);
-          assertThat(body.getString("_ref")).isEqualTo("YVSglL8OxJeUDkw");
           testContext.completeNow();
         } else {
           info("error {}", ar.cause());
