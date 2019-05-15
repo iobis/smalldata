a9f2ee8ef55de4d0a7303df01d6e07b32f1f5e7d
Oleg Tsvetkov <olecve@gmail.com>
2019-05-15 22:47:54 +0300

    Change machineObservation to be second option

--- occurrences-app/src/components/pages/Occurrence/OccurrenceData/OccurrenceData.js
+++ occurrences-app/src/components/pages/Occurrence/OccurrenceData/OccurrenceData.js
@@ -6,7 +6,7 @@ import React from 'react'
 import ScientificNameInput from './ScientificNameInput'
 import { useTranslation } from 'react-i18next'
 
-const basisOfRecordOptions = ['humanObservation', 'fossilSpecimen', 'livingSpecimen', 'machineObservation', 'preservedSpecimen']
+const basisOfRecordOptions = ['humanObservation', 'machineObservation', 'fossilSpecimen', 'livingSpecimen', 'preservedSpecimen']
 const lifestageOptions = ['larva', 'juvenile', 'adult', 'unspecified']
 const occurrenceStatusOptions = ['absent', 'present']
 const sexOptions = ['male', 'female', 'unspecified']
