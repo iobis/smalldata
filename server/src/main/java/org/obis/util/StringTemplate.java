6ad46e2d269cb261f8ab84f4267ee6a2b325194d
Oleg Tsvetkov <olecve@gmail.com>
2019-05-23 21:48:35 +0300

    Simplify StringTemplate

--- server/src/main/java/org/obis/util/StringTemplate.java
+++ server/src/main/java/org/obis/util/StringTemplate.java
@@ -5,11 +5,9 @@ import java.util.regex.Pattern;
 
 public class StringTemplate {
 
-  public static String interpolate(String template, Map<String, ? extends Object> values) {
+  public static String interpolate(String template, Map<String, ?> values) {
     var result = template;
-    var it = values.entrySet().iterator();
-    while (it.hasNext()) {
-      var entry = it.next();
+    for (Map.Entry<String, ?> entry : values.entrySet()) {
       result = Pattern.compile(Pattern.quote("#{" + entry.getKey() + "}"))
         .matcher(result)
         .replaceAll(entry.getValue().toString());
