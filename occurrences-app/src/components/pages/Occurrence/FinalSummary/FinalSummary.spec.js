fcb42da52969f02d114befb4634a15c40ae8ecbf
Oleg Tsvetkov <olecve@gmail.com>
2019-05-27 21:09:30 +0300

    Add unit test for onSubmitClick

--- occurrences-app/src/components/pages/Occurrence/FinalSummary/FinalSummary.spec.js
+++ occurrences-app/src/components/pages/Occurrence/FinalSummary/FinalSummary.spec.js
@@ -9,20 +9,34 @@ describe('FinalSummary', () => {
   })
 
   describe('when clicking change button', () => {
-    [{ className: 'select-dataset', event: 'selectDataset' },
-      { className: 'occurrence-data', event: 'occurrenceData' },
-      { className: 'location-data', event: 'locationData' },
-      { className: 'observation-data', event: 'observationData' },
-      { className: 'measurement-or-fact', event: 'measurementOrFact' },
-      { className: 'darwin-core-fields', event: 'darwinCoreFields' }
-    ].forEach(({ className, event }) => {
-      it(`calls onChangeClick handler with ${event} when clicking change button in ${className}`, () => {
+    [{ className: 'select-dataset', eventString: 'selectDataset' },
+      { className: 'occurrence-data', eventString: 'occurrenceData' },
+      { className: 'location-data', eventString: 'locationData' },
+      { className: 'observation-data', eventString: 'observationData' },
+      { className: 'measurement-or-fact', eventString: 'measurementOrFact' },
+      { className: 'darwin-core-fields', eventString: 'darwinCoreFields' }
+    ].forEach(({ className, eventString }) => {
+      it(`calls onChangeClick handler with ${eventString} when clicking change button in ${className}`, () => {
         const onChangeClick = jest.fn()
         const wrapper = mount(createComponent({ onChangeClick }))
         wrapper.find('.' + className + ' .change-button').simulate('click')
         expect(onChangeClick).toHaveBeenCalledTimes(1)
-        expect(onChangeClick).toBeCalledWith(event)
+        expect(onChangeClick).toBeCalledWith(eventString)
       })
+    })
+  })
+
+  describe('when clicking submit button', () => {
+    it('calls onSubmitClick handler', () => {
+      const onSubmitClick = jest.fn()
+      const wrapper = mount(createComponent({ onSubmitClick }))
+      expect(wrapper.find('.submit-entry-button .button')).toHaveLength(2)
+
+      wrapper.find('.submit-entry-button .button').at(0).simulate('click')
+      expect(onSubmitClick).toHaveBeenCalledTimes(1)
+
+      wrapper.find('.submit-entry-button .button').at(1).simulate('click')
+      expect(onSubmitClick).toHaveBeenCalledTimes(2)
     })
   })
 })
