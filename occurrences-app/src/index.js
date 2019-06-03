ab37bd981641c41efe8d22b12bd36a42a0967800
Oleg Tsvetkov <olecve@gmail.com>
2019-06-03 19:56:36 +0300

    Add whatwg-fetch

--- occurrences-app/src/index.js
+++ occurrences-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import 'whatwg-fetch'
 
 const rootEl = document.getElementById('root')
 
