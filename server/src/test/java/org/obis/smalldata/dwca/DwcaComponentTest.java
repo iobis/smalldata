f06938d8a9c65b2ea0b44abeede06cde039f100d
Oleg Tsvetkov <olecve@gmail.com>
2019-06-05 00:24:19 +0300

    Improve test coverage

--- server/src/test/java/org/obis/smalldata/dwca/DwcaComponentTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/DwcaComponentTest.java
@@ -151,6 +151,7 @@ public class DwcaComponentTest {
           JsonArray records = ar.result().body();
           assertThat(records).hasSize(1);
           JsonObject result = records.getJsonObject(0);
+          assertThat(result.getMap()).containsOnlyKeys("dataset", "dwcRecords", "dwcaId");
           assertThat(result.getString("dataset")).isEqualTo("ntDOtUc7XsRrIus");
           JsonObject dwcRecords = result.getJsonObject("dwcRecords");
           assertThat(dwcRecords.getString(KEY_CORE)).isEqualTo("occurrence");
