263bbd5183f843798a4f20aa7815f0a7a87c6ef7
kurt.sys <2430465+kurt-o-sys@users.noreply.github.com>
2019-06-04 14:12:58 +0200

    fix same records returned, implement get by id returning 1 dwca record

--- server/src/main/java/org/obis/smalldata/webapi/DwcaRecordsHandler.java
+++ server/src/main/java/org/obis/smalldata/webapi/DwcaRecordsHandler.java
@@ -11,38 +11,90 @@ import lombok.Value;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
 import static org.pmw.tinylog.Logger.info;
 
 public class DwcaRecordsHandler {
 
+  private static final String MIME_APPLICATION_JSON = "application/json";
+  private static final String DOES_NOT_EXIST = "' does not exist.";
+
   public static void put(RoutingContext context) {
-    info("context: {}", context.request());
-    info("host: {}", context.request().host());
     context
       .response()
-      .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+      .putHeader(HttpHeaders.CONTENT_TYPE, MIME_APPLICATION_JSON)
       .end(new JsonObject().put("occurenceID", "some ID").encode());
   }
 
   public static void post(RoutingContext context) {
-    var datasetRef = context.request().getParam("datasetRef");
-    var userRef = context.request().getParam("userRef");
-    var messages = new DwcaBodyValidator(context.vertx().eventBus(), datasetRef, userRef).validate(context.getBodyAsJson());
-    messages.setHandler(arMessages -> {
-      info(arMessages);
-      if (arMessages.result().isEmpty()) {
-        context.vertx().eventBus().<JsonObject>send(
+    actionOnDwcaRecord(context,
+      (datasetRef, userRef) ->
+        new DwcaBodyValidator(context.vertx().eventBus(), datasetRef, userRef).validate(context.getBodyAsJson()),
+      (datasetRef, userRef) ->
+      context.vertx().eventBus().<JsonObject>send(
+        "dwca.record",
+        new JsonObject()
+          .put("action", "insert")
+          .put("datasetRef", datasetRef)
+          .put("userRef", userRef)
+          .put("record", context.getBodyAsJson()),
+        ar -> context
+          .response()
+          .putHeader(HttpHeaders.CONTENT_TYPE, MIME_APPLICATION_JSON)
+          .end(ar.result().body().encode())));
+  }
+
+  public static void get(RoutingContext context) {
+    actionOnDwcaRecord(context,
+      (datasetRef, userRef) ->
+        new DwcaBodyValidator(context.vertx().eventBus(), datasetRef, userRef).validate(),
+      (datasetRef, userRef) ->
+        context.vertx().eventBus().<JsonArray>send(
           "dwca.record",
           new JsonObject()
-            .put("action", "insert")
-            .put("datasetRef", datasetRef)
-            .put("userRef", userRef)
-            .put("record", context.getBodyAsJson()),
-          ar -> context
-            .response()
-            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-            .end(ar.result().body().encode()));
+            .put("action", "find")
+            .put("query", new JsonObject().put("dwcRecord.id", context.request().getParam("dwcaId"))),
+          ar -> {
+            var result = ar.result().body();
+            info(result.encodePrettily());
+            info(result.size());
+            switch (result.size()) {
+              case 0:
+                context
+                  .response()
+                  .putHeader(HttpHeaders.CONTENT_TYPE, MIME_APPLICATION_JSON)
+                  .setStatusCode(404)
+                  .end(new JsonObject().put("error", "record "
+                    + context.request().getParam("dwcaId")
+                    + DOES_NOT_EXIST).encode());
+                break;
+              case 1:
+                context
+                  .response()
+                  .putHeader(HttpHeaders.CONTENT_TYPE, MIME_APPLICATION_JSON)
+                  .end(ar.result().body().getJsonObject(0).encode());
+                break;
+              default:
+                context
+                  .response()
+                  .putHeader(HttpHeaders.CONTENT_TYPE, MIME_APPLICATION_JSON)
+                  .setStatusCode(400)
+                  .end(new JsonObject().put("error", "illegal db state: more than 1 record with this id").encode());
+                break;
+            }
+          }));
+  }
+
+  private static void actionOnDwcaRecord(RoutingContext context,
+                                         BiFunction<String, String, Future<List<String>>> messages,
+                                         BiConsumer<String, String> succesHandler) {
+    var datasetRef = context.request().getParam("datasetRef");
+    var userRef = context.request().getParam("userRef");
+    messages.apply(datasetRef, userRef).setHandler(arMessages -> {
+      if (arMessages.result().isEmpty()) {
+        succesHandler.accept(datasetRef, userRef);
       } else {
         var jsonMessages = new JsonArray();
         arMessages.result().forEach(jsonMessages::add);
@@ -90,10 +142,27 @@ public class DwcaRecordsHandler {
     Future<Boolean> datasetExists() {
       var exists = Future.<Boolean>future();
       eventBus.<Boolean>send("datasets.exists", datasetRef, ar -> {
-        info(ar);
         exists.complete(ar.result().body());
       });
       return exists;
+    }
+
+    Future<List<String>> validate() {
+      var result = Future.<List<String>>future();
+
+      CompositeFuture.all(datasetExists(), userExists()).setHandler(ar -> {
+        var messages = new ArrayList<String>();
+        var datasetExists = (Boolean) ar.result().list().get(0);
+        var userExists = (Boolean) ar.result().list().get(1);
+        if (!datasetExists) {
+          messages.add("dataset with ref '" + datasetRef + DOES_NOT_EXIST);
+        }
+        if (!userExists) {
+          messages.add("user with ref '" + userRef + DOES_NOT_EXIST);
+        }
+        result.complete(messages);
+      });
+      return result;
     }
 
     Future<List<String>> validate(JsonObject dwcaRecord) {
@@ -109,10 +178,10 @@ public class DwcaRecordsHandler {
           messages.add("core table '" + coreTable + "' can have only 1 record.");
         }
         if (!datasetExists) {
-          messages.add("dataset with ref '" + datasetRef + "' does not exist.");
+          messages.add("dataset with ref '" + datasetRef + DOES_NOT_EXIST);
         }
         if (!userExists) {
-          messages.add("user with ref '" + userRef + "' does not exist.");
+          messages.add("user with ref '" + userRef + DOES_NOT_EXIST);
         }
         result.complete(messages);
       });
