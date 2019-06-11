f37df5143a04e0902ebb359551e944771d510636
Oleg Tsvetkov <olecve@gmail.com>
2019-06-12 00:26:24 +0300

    Update ManageDatasetPage component and clean up styles

--- admin-app/src/i18n/en.js
+++ admin-app/src/i18n/en.js
@@ -1,11 +1,21 @@
 export default {
   translation: {
-    navbar: {
+    navbar:            {
       manageDataset: 'MANAGE DATASET',
       manageUsers:   'MANAGE USERS',
       inputData:     'INPUT DATA',
       logout:        'logout',
       login:         'login'
+    },
+    manageDatasetPage: {
+      inputNew:              'Input New Dataset',
+      managePreviousEntries: 'Manage Previous Entries',
+      or:                    'OR',
+      table:                 {
+        title:        'Title',
+        organization: 'Organization',
+        licence:      'Licence'
+      }
     }
   }
 }
