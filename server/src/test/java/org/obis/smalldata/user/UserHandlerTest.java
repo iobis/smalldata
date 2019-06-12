c976d8ab2a5c8b6e240e43cb5c9a76bb1dd533d6
kurt-o-sys <ikh0uvanLieftie>
2019-06-12 17:22:29 +0200

    finish users api

--- server/src/test/java/org/obis/smalldata/user/UserHandlerTest.java
+++ server/src/test/java/org/obis/smalldata/user/UserHandlerTest.java
@@ -6,25 +6,31 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.obis.smalldata.testutil.TestDb;
+import org.obis.smalldata.util.Collections;
 
+import java.util.AbstractMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.pmw.tinylog.Logger.info;
 
 @ExtendWith(VertxExtension.class)
 public class UserHandlerTest {
 
   private static final String KEY_ACTION = "action";
-  private static TestDb testDb;
+  private static final String KEY_DATASET_REFS = "dataset_refs";
+  private static final String KEY_EMAIL_ADDRESS = "emailAddress";
+  private static final String KEY_REF = "_ref";
+  private TestDb testDb;
 
-  @BeforeAll
-  public static void deployVerticle(Vertx vertx, VertxTestContext testContext) {
+  @BeforeEach
+  public void deployVerticle(Vertx vertx, VertxTestContext testContext) {
     testDb = new TestDb();
     testDb.init(vertx);
     vertx.sharedData().getLocalMap("settings")
@@ -40,8 +46,8 @@ public class UserHandlerTest {
       testContext.succeeding(id -> testContext.completeNow()));
   }
 
-  @AfterAll
-  public static void stop() {
+  @AfterEach
+  public void stop() {
     testDb.shutDown();
   }
 
@@ -57,11 +63,11 @@ public class UserHandlerTest {
           assertThat(records).hasSize(2);
           JsonObject userjson = records.stream()
             .map(JsonObject.class::cast)
-            .filter(user -> user.getString("_ref").equals("ovZTtaOJZ98xDDY"))
+            .filter(user -> user.getString(KEY_REF).equals("ovZTtaOJZ98xDDY"))
             .findFirst()
             .get();
-          assertThat(userjson.getJsonArray("dataset_refs")).hasSize(4);
-          assertThat(userjson.getJsonArray("dataset_refs"))
+          assertThat(userjson.getJsonArray(KEY_DATASET_REFS)).hasSize(4);
+          assertThat(userjson.getJsonArray(KEY_DATASET_REFS))
             .containsExactly("wEaBfmFyQhYCdsk", "ntDOtUc7XsRrIus", "PoJnGNMaxsupE4w", "NnqVLwIyPn-nRkc");
           testContext.completeNow();
         } else {
@@ -76,18 +82,18 @@ public class UserHandlerTest {
     vertx.eventBus().<JsonArray>send(
       "users",
       new JsonObject(Map.of(KEY_ACTION, "find",
-      "query", new JsonObject().put("emailAddress", "some.user@domain.org"))),
+      "query", new JsonObject().put(KEY_EMAIL_ADDRESS, "some.user@domain.org"))),
       ar -> {
         if (ar.succeeded()) {
           JsonArray records = ar.result().body();
           assertThat(records).hasSize(1);
           JsonObject userjson = records.stream()
             .map(JsonObject.class::cast)
-            .filter(user -> user.getString("_ref").equals("ovZTtaOJZ98xDDY"))
+            .filter(user -> user.getString(KEY_REF).equals("ovZTtaOJZ98xDDY"))
             .findFirst()
             .get();
-          assertThat(userjson.getJsonArray("dataset_refs")).hasSize(4);
-          assertThat(userjson.getJsonArray("dataset_refs"))
+          assertThat(userjson.getJsonArray(KEY_DATASET_REFS)).hasSize(4);
+          assertThat(userjson.getJsonArray(KEY_DATASET_REFS))
             .containsExactly("wEaBfmFyQhYCdsk", "ntDOtUc7XsRrIus", "PoJnGNMaxsupE4w", "NnqVLwIyPn-nRkc");
           testContext.completeNow();
         } else {
@@ -96,4 +102,50 @@ public class UserHandlerTest {
       });
   }
 
+  @Test
+  @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
+  void insertUser(Vertx vertx, VertxTestContext testContext) {
+    vertx.eventBus().<JsonObject>send(
+      Collections.USERS.dbName(),
+      new JsonObject(Map.of(KEY_ACTION, "insert",
+      "user", new JsonObject()
+          .put(KEY_EMAIL_ADDRESS, "my.user@domain.org"))),
+      ar -> {
+        if (ar.succeeded()) {
+          assertThat(ar.succeeded()).isTrue();
+          var json = ar.result().body();
+          assertThat(json).contains(new AbstractMap.SimpleEntry(KEY_EMAIL_ADDRESS, "my.user@domain.org"));
+          assertThat(json.containsKey(KEY_REF)).isTrue();
+          assertThat(json.containsKey("_id")).isFalse();
+          testContext.completeNow();
+        } else {
+          testContext.failNow(ar.cause());
+        }
+      });
+  }
+
+  @Test
+  @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
+  void replaceUser(Vertx vertx, VertxTestContext testContext) {
+    vertx.eventBus().<JsonObject>send(
+      Collections.USERS.dbName(),
+      new JsonObject(Map.of(
+        KEY_ACTION, "replace",
+        "userRef", "FsfEMwhUTO_8I68",
+        "user", new JsonObject().put(KEY_EMAIL_ADDRESS, "my.otheruser@domain.com"))),
+      ar -> {
+        if (ar.succeeded()) {
+          info(ar.cause());
+          assertThat(ar.succeeded()).isTrue();
+          var json = ar.result().body();
+          info(json);
+          assertThat(json).contains(new AbstractMap.SimpleEntry(KEY_EMAIL_ADDRESS, "my.otheruser@domain.com"));
+          assertThat(json.containsKey(KEY_REF)).isTrue();
+          assertThat(json.containsKey("_id")).isFalse();
+          testContext.completeNow();
+        } else {
+          testContext.failNow(ar.cause());
+        }
+      });
+  }
 }
--- server/src/test/java/org/obis/smalldata/webapi/UsersHandlerTest.java
+++ server/src/test/java/org/obis/smalldata/webapi/UsersHandlerTest.java
@@ -20,13 +20,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.pmw.tinylog.Logger.info;
 
 @ExtendWith(VertxExtension.class)
-public class UserHandlerTest {
+public class UsersHandlerTest {
 
   private static final int HTTP_PORT = 8080;
   private static final JsonObject CONFIG = new JsonObject().put("port", HTTP_PORT);
   private static final String LOCALHOST = "localhost";
   private static final String URL_API_USERS = "/api/users/";
   private static final String KEY_USERS_REF = "_ref";
+  private static final String KEY_EMAIL_ADDRESS = "emailAddress";
+  private static final String DEFAULT_EMAIL_ADDRESS = "my.name@organization.ours";
 
   @BeforeEach
   void deployVerticle(Vertx vertx, VertxTestContext testContext) {
@@ -38,7 +40,7 @@ public class UserHandlerTest {
   }
 
   @Test
-  @DisplayName("dwca record get handler")
+  @DisplayName("users get handler")
   @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
   void testGetHandler(Vertx vertx, VertxTestContext context) {
 
@@ -58,5 +60,53 @@ public class UserHandlerTest {
         assertThat(json.getJsonObject(0).getString(KEY_USERS_REF)).isEqualTo("some-user-ref");
         context.completeNow();
       });
+  }
+
+  @Test
+  @DisplayName("users post handler")
+  @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
+  void testPostHandler(Vertx vertx, VertxTestContext context) {
+    var client = WebClient.create(vertx);
+    var url = URL_API_USERS;
+    vertx.eventBus().localConsumer(
+      "users",
+      message -> message.reply(((JsonObject) message.body()).getJsonObject("user")));
+    client
+      .post(HTTP_PORT, LOCALHOST, url)
+      .as(BodyCodec.jsonObject())
+      .sendJson(
+        new JsonObject()
+          .put(KEY_EMAIL_ADDRESS, DEFAULT_EMAIL_ADDRESS),
+        ar -> {
+          assertThat(ar.succeeded()).isTrue();
+          var json = ar.result().body();
+          assertThat(json.containsKey(KEY_EMAIL_ADDRESS)).isTrue();
+          assertThat(json.getString(KEY_EMAIL_ADDRESS)).isEqualTo(DEFAULT_EMAIL_ADDRESS);
+          context.completeNow();
+        });
+  }
+
+  @Test
+  @DisplayName("users put handler")
+  @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
+  void testPutHandler(Vertx vertx, VertxTestContext context) {
+    var client = WebClient.create(vertx);
+    var url = URL_API_USERS;
+    vertx.eventBus().localConsumer(
+      "users",
+      message -> message.reply(((JsonObject) message.body()).getJsonObject("user")));
+    client
+      .put(HTTP_PORT, LOCALHOST, url)
+      .as(BodyCodec.jsonObject())
+      .sendJson(
+        new JsonObject()
+          .put(KEY_EMAIL_ADDRESS, DEFAULT_EMAIL_ADDRESS),
+        ar -> {
+          assertThat(ar.succeeded()).isTrue();
+          var json = ar.result().body();
+          assertThat(json.containsKey(KEY_EMAIL_ADDRESS)).isTrue();
+          assertThat(json.getString(KEY_EMAIL_ADDRESS)).isEqualTo(DEFAULT_EMAIL_ADDRESS);
+          context.completeNow();
+        });
   }
 }
