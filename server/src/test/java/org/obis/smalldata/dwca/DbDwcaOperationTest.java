c0c48f410e6de56c656612c9635712f75e81bb7d
Oleg Tsvetkov <olecve@gmail.com>
2019-06-16 12:10:53 +0300

    Fix PMD errors

--- server/src/test/java/org/obis/smalldata/dwca/DbDwcaOperationTest.java
+++ server/src/test/java/org/obis/smalldata/dwca/DbDwcaOperationTest.java
@@ -18,14 +18,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(VertxExtension.class)
 class DbDwcaOperationTest {
-  private static final String DWC_TABLE = "dwcTable";
+  private static final String DATASET_REF_DEFAULT_VALUE = "NnqVLwIyPn-nRkc";
+  private static final String DATASET_REF_KEY = "dataset_ref";
   private static final String DWC_RECORD = "dwcRecord";
-  private static final String DEFAULT_DATASET_REF = "NnqVLwIyPn-nRkc";
-  private static final String DEFAULT_USER_REF = "ovZTtaOJZ98xDDY";
-  private static final String KEY_DATASET_REF = "dataset_ref";
-  private static final String KEY_IOBIS = "iobis";
-  private static final String KEY_USER_REF = "user_ref";
-  private static final JsonObject DATASET_QUERY = new JsonObject().put(KEY_DATASET_REF, DEFAULT_DATASET_REF);
+  private static final String DWC_TABLE = "dwcTable";
+  private static final String IOBIS_KEY = "iobis";
+  private static final String USER_REF_DEFAULT_VALUE = "ovZTtaOJZ98xDDY";
+  private static final String USER_REF_KEY = "user_ref";
+  private static final JsonObject DATASET_QUERY = new JsonObject().put(DATASET_REF_KEY, DATASET_REF_DEFAULT_VALUE);
 
   private static TestDb testDb;
   private static MongoClient mongoClient;
@@ -64,8 +64,8 @@ class DbDwcaOperationTest {
       assertThat(result)
         .hasSize(642)
         .allSatisfy(dwca -> {
-          assertThat(dwca.getMap()).containsOnlyKeys("_ref", "user_ref", "_id", "dataset_ref", DWC_TABLE, DWC_RECORD);
-          assertThat(dwca.getJsonObject(DWC_RECORD).getMap()).containsOnlyKeys("id", "tdwg", "purl", "iobis");
+          assertThat(dwca.getMap()).containsOnlyKeys("_ref", USER_REF_KEY, "_id", DATASET_REF_KEY, DWC_TABLE, DWC_RECORD);
+          assertThat(dwca.getJsonObject(DWC_RECORD).getMap()).containsOnlyKeys("id", "tdwg", "purl", IOBIS_KEY);
         });
       testContext.completeNow();
     });
@@ -77,12 +77,12 @@ class DbDwcaOperationTest {
       DATASET_QUERY,
       new JsonObject()
         .put("_ref", true)
-        .put("user_ref", false));
+        .put(USER_REF_KEY, false));
     dwcaRecords.setHandler(ar -> {
       assertThat(ar.result())
         .hasSize(642)
         .allSatisfy(dwca -> {
-          assertThat(dwca.getMap()).containsOnlyKeys("_ref", "user_ref", "_id", "dataset_ref", DWC_TABLE, DWC_RECORD);
+          assertThat(dwca.getMap()).containsOnlyKeys("_ref", USER_REF_KEY, "_id", DATASET_REF_KEY, DWC_TABLE, DWC_RECORD);
           assertThat(dwca.getJsonObject(DWC_RECORD).getMap()).containsOnlyKeys("id");
         });
       testContext.completeNow();
@@ -98,7 +98,7 @@ class DbDwcaOperationTest {
       assertThat(ar.result())
         .hasSize(642)
         .allSatisfy(dwca -> {
-          assertThat(dwca.getMap()).containsOnlyKeys("user_ref", "_id", "dataset_ref", DWC_TABLE, DWC_RECORD);
+          assertThat(dwca.getMap()).containsOnlyKeys(USER_REF_KEY, "_id", DATASET_REF_KEY, DWC_TABLE, DWC_RECORD);
           assertThat(dwca.getJsonObject(DWC_RECORD).getMap()).containsOnlyKeys("id", "tdwg");
         });
       testContext.completeNow();
@@ -107,7 +107,7 @@ class DbDwcaOperationTest {
 
   @Test
   void findDwcaRecordsForUnknownDatasetRef(VertxTestContext testContext) {
-    var dwcaRecords = dbDwcaOperation.findDwcaRecords(new JsonObject().put(KEY_DATASET_REF, "unknown"));
+    var dwcaRecords = dbDwcaOperation.findDwcaRecords(new JsonObject().put(DATASET_REF_KEY, "unknown"));
     dwcaRecords.setHandler(ar -> {
       var result = ar.result();
       assertThat(result).isEmpty();
@@ -117,7 +117,7 @@ class DbDwcaOperationTest {
 
   @Test
   void findDwcaRecordsForUserRef(VertxTestContext testContext) {
-    var dwcaRecords = dbDwcaOperation.findDwcaRecords(new JsonObject().put(KEY_USER_REF, DEFAULT_USER_REF));
+    var dwcaRecords = dbDwcaOperation.findDwcaRecords(new JsonObject().put(USER_REF_KEY, USER_REF_DEFAULT_VALUE));
     dwcaRecords.setHandler(ar -> {
       var result = ar.result();
       assertThat(result).hasSize(2955);
@@ -127,7 +127,7 @@ class DbDwcaOperationTest {
 
   @Test
   void findDwcaRecordsForUnknownUserRef(VertxTestContext testContext) {
-    var dwcaRecords = dbDwcaOperation.findDwcaRecords(new JsonObject().put(KEY_USER_REF, "unknown"));
+    var dwcaRecords = dbDwcaOperation.findDwcaRecords(new JsonObject().put(USER_REF_KEY, "unknown"));
     dwcaRecords.setHandler(ar -> {
       var result = ar.result();
       assertThat(result).isEmpty();
@@ -137,7 +137,7 @@ class DbDwcaOperationTest {
 
   @Test
   void findDataset(VertxTestContext testContext) {
-    var dataset = dbDwcaOperation.findDataset(DEFAULT_DATASET_REF);
+    var dataset = dbDwcaOperation.findDataset(DATASET_REF_DEFAULT_VALUE);
     dataset.setHandler(ar -> {
       var result = ar.result();
       assertThat(result).hasSize(12);
@@ -171,26 +171,26 @@ class DbDwcaOperationTest {
     var records = List.of(
       new JsonObject()
         .put(DWC_TABLE, "occurrence")
-        .put(KEY_USER_REF, DEFAULT_USER_REF)
-        .put(KEY_DATASET_REF, DEFAULT_DATASET_REF)
+        .put(USER_REF_KEY, USER_REF_DEFAULT_VALUE)
+        .put(DATASET_REF_KEY, DATASET_REF_DEFAULT_VALUE)
         .put(DWC_RECORD, new JsonObject()
           .put("id", dwcaId)
-          .put(KEY_IOBIS, new JsonObject())),
+          .put(IOBIS_KEY, new JsonObject())),
       new JsonObject()
         .put(DWC_TABLE, "emof")
-        .put(KEY_USER_REF, DEFAULT_USER_REF)
-        .put(KEY_DATASET_REF, DEFAULT_DATASET_REF)
+        .put(USER_REF_KEY, USER_REF_DEFAULT_VALUE)
+        .put(DATASET_REF_KEY, DATASET_REF_DEFAULT_VALUE)
         .put(DWC_RECORD, new JsonObject()
           .put("id", dwcaId)
           .put("purl", new JsonObject())
-          .put(KEY_IOBIS, new JsonObject())),
+          .put(IOBIS_KEY, new JsonObject())),
       new JsonObject()
         .put(DWC_TABLE, "emof")
-        .put(KEY_USER_REF, DEFAULT_USER_REF)
-        .put(KEY_DATASET_REF, DEFAULT_DATASET_REF)
+        .put(USER_REF_KEY, USER_REF_DEFAULT_VALUE)
+        .put(DATASET_REF_KEY, DATASET_REF_DEFAULT_VALUE)
         .put(DWC_RECORD, new JsonObject()
           .put("id", dwcaId)
-          .put(KEY_IOBIS, new JsonObject())));
+          .put(IOBIS_KEY, new JsonObject())));
     var result = dbDwcaOperation.putDwcaRecord(dwcaId, records);
     result.setHandler(arOperation -> {
       var operationResult = arOperation.result();
@@ -204,7 +204,7 @@ class DbDwcaOperationTest {
           var foundRecords = arFind.result();
           assertThat(foundRecords).hasSize(3);
           foundRecords.forEach(found -> {
-            assertThat(found.getJsonObject(DWC_RECORD).getJsonObject(KEY_IOBIS)).isEqualTo(new JsonObject());
+            assertThat(found.getJsonObject(DWC_RECORD).getJsonObject(IOBIS_KEY)).isEqualTo(new JsonObject());
           });
           testContext.completeNow();
         });
