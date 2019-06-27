027cbd70dea30a9441a3e400a89cdd8b1c9742df
Jill VandenDriessche <jill.vandendriessche@howest.be>
2019-06-27 22:11:59 +0200

    update snapshots
    Signed-off-by: Jill VandenDriessche <jill.vandendriessche@howest.be>

--- occurrences-app/src/components/pages/Occurrence/StepHeaders/ActiveStepHeader.spec.js
+++ occurrences-app/src/components/pages/Occurrence/StepHeaders/ActiveStepHeader.spec.js
@@ -6,6 +6,7 @@ describe('ActiveStepHeader', () => {
   it('renders correctly', () => {
     expect(renderer.create(
       <ActiveStepHeader
+        onContinueButtonClick={() => {}}
         onStepTitleClick={() => {}}
         stepDescription="step description"
         stepTitle="step title"/>
