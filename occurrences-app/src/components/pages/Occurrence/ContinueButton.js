75a7825bd9b03e349128c978e33f7228d30511e0
Oleg Tsvetkov <olecve@gmail.com>
2019-06-27 21:36:26 +0300

    Fix typo

--- occurrences-app/src/components/pages/Occurrence/ContinueButton.js
+++ occurrences-app/src/components/pages/Occurrence/ContinueButton.js
@@ -1,5 +1,5 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 export default function ContinueButton({ name, value, wrapperClassName, nextStepHandler }) {
--- occurrences-app/src/components/pages/Occurrence/ContinueButton.spec.js
+++ occurrences-app/src/components/pages/Occurrence/ContinueButton.spec.js
@@ -4,29 +4,29 @@ import { mount } from 'enzyme'
 
 describe('ContinueButton', () => {
   it('renders correctly', () => {
-    expect(mount(createCompontent())).toMatchSnapshot()
+    expect(mount(createComponent())).toMatchSnapshot()
   })
 
   it('renders a given value correctly', () => {
-    const wrapper = mount(createCompontent({ value: 'Go to next page' }))
+    const wrapper = mount(createComponent({ value: 'Go to next page' }))
     expect(wrapper.find('button').text()).toBe('Go to next page')
   })
 
   it('applies given CSS classes to wrapper correctly', () => {
-    const wrapper = mount(createCompontent({ wrapperClassName: 'my-own-class' }))
+    const wrapper = mount(createComponent({ wrapperClassName: 'my-own-class' }))
     expect(wrapper.find('div').at(0).hasClass('my-own-class')).toBe(true)
   })
 
   it('calls nextStepHandler', () => {
     const nextStepHandler = jest.fn()
-    const wrapper = mount(createCompontent({ nextStepHandler: nextStepHandler }))
+    const wrapper = mount(createComponent({ nextStepHandler: nextStepHandler }))
     wrapper.find('button').at(0).simulate('click')
 
     expect(nextStepHandler).toHaveBeenCalledTimes(1)
   })
 })
 
-function createCompontent(props) {
+function createComponent(props) {
   const defaultProps = {
     name:             'continue',
     nextStepHandler:  jest.fn(),
