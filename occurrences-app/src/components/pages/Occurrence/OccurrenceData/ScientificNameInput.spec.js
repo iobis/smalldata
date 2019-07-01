7d50d6a41c40d659cb4f94104e613dd852643186
Oleg Tsvetkov <olecve@gmail.com>
2019-07-01 19:21:58 +0300

    Update ScientificNameInput state when it receives new props

--- occurrences-app/src/components/pages/Occurrence/OccurrenceData/ScientificNameInput.spec.js
+++ occurrences-app/src/components/pages/Occurrence/OccurrenceData/ScientificNameInput.spec.js
@@ -6,6 +6,15 @@ describe('ScientificNameInput', () => {
   it('renders correctly', () => {
     expect(mount(createComponent())).toMatchSnapshot()
   })
+
+  it('updates values when updates props', () => {
+    const wrapper = mount(createComponent({ scientificName: 'name-1' }))
+    expect(wrapper.find('input').props().value).toBe('name-1')
+
+    wrapper.setProps({ scientificName: 'name-100500' })
+    wrapper.update()
+    expect(wrapper.find('input').props().value).toBe('name-100500')
+  })
 })
 
 function createComponent(props) {
