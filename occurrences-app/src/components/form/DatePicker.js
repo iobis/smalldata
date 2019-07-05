470e8ad24db3a295c1dd163db24ca5939a88699e
Oleg Tsvetkov <olecve@gmail.com>
2019-07-05 22:55:13 +0300

    Fix calendar logic to update value

--- occurrences-app/src/components/form/DatePicker.js
+++ occurrences-app/src/components/form/DatePicker.js
@@ -1,23 +1,37 @@
 import bulmaCalendar from 'bulma-calendar'
 import PropTypes from 'prop-types'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { differenceInCalendarDays } from 'date-fns'
 
 export default function DatePicker({ onChange, value }) {
   const datePickerEl = useRef()
+  const [instance, setInstance] = useState(null)
+  const [selectedDate, setSelectedDate] = useState(new Date(value))
+
+  function handleValueChange(e) {
+    const newDate = new Date(e.data.value())
+    setSelectedDate(newDate)
+    onChange(newDate)
+  }
 
   useEffect(() => {
-    const calendar = bulmaCalendar.attach(datePickerEl.current, {
-      dateFormat:         'D MMMM YYYY',
-      displayMode:        'default',
-      showFooter:         false,
-      showHeader:         false,
-      startDate:          value !== null ? new Date(value) : null,
-      toggleOnInputClick: true,
-      type:               'date'
-    })
-    calendar[0].on('select', (e) => onChange(new Date(e.data.value())))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    if (instance === null) {
+      const calendar = bulmaCalendar.attach(datePickerEl.current, {
+        dateFormat:         'D MMMM YYYY',
+        displayMode:        'default',
+        showFooter:         false,
+        showHeader:         false,
+        startDate:          value !== null ? new Date(value) : null,
+        toggleOnInputClick: true,
+        type:               'date'
+      })
+      const instance = calendar[0]
+      instance.on('select', handleValueChange)
+      setInstance(instance)
+    } else if (differenceInCalendarDays(new Date(value), selectedDate) !== 0) {
+      instance.value(value)
+    }
+  }, [value])
 
   return <input className="input" ref={datePickerEl} type="date"/>
 }
