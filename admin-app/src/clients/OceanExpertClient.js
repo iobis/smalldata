7a941e23c9622abfa01630ef27015d3766924048
Oleg Tsvetkov <olecve@gmail.com>
2019-07-09 17:11:06 +0300

    Add simple OceanExpertClient

---
+++ admin-app/src/clients/OceanExpertClient.js
@@ -0,0 +1,8 @@
+export function getByName(name) {
+  const defaultResult = []
+  if (!name || !name.trim()) return defaultResult
+  const url = 'https://www.oceanexpert.net/api/v1/advanceSearch/search.json?action=browse&type=all&query=' + name.trim()
+  return fetch(url)
+    .then(response => response.json())
+    .then(json => json.results.data)
+}
---
+++ admin-app/src/clients/OceanExpertClient.spec.js
@@ -0,0 +1,76 @@
+import { getByName } from './OceanExpertClient'
+
+describe('OceanExpertClient', () => {
+  describe('getByName', () => {
+    beforeEach(() => {
+      global.fetch = jest.fn().mockImplementation(() =>
+        new Promise((resolve) => {
+          resolve({
+            json: () => ({
+              results: { data: [createUser()] }
+            })
+          })
+        })
+      )
+    })
+
+    afterEach(() => {
+      global.fetch.mockRestore()
+    })
+
+    it('for some name', async() => {
+      const response = await getByName('Firstname')
+
+      expect(response).toEqual([createUser()])
+      expect(fetch).toHaveBeenCalledTimes(1)
+      expect(fetch).toHaveBeenCalledWith('https://www.oceanexpert.net/api/v1/advanceSearch/search.json?action=browse&type=all&query=Firstname')
+    })
+
+    it('for name with extra whitespaces spaces', async() => {
+      const response = await getByName('  Firstname   ')
+
+      expect(response).toEqual([createUser()])
+      expect(fetch).toHaveBeenCalledTimes(1)
+      expect(fetch).toHaveBeenCalledWith('https://www.oceanexpert.net/api/v1/advanceSearch/search.json?action=browse&type=all&query=Firstname')
+    })
+
+    it('for name with spaces only', async() => {
+      const response = await getByName('     ')
+
+      expect(response).toEqual([])
+      expect(fetch).toHaveBeenCalledTimes(0)
+    })
+  })
+})
+
+function createUser() {
+  return {
+    'type':          'experts',
+    'status':        '1',
+    'id_ind':        '12345',
+    'name':          'Firstname Lastname',
+    'fname':         'Firstname',
+    'mname':         '',
+    'sname':         'Lastname',
+    'inst_name':     'UNESCO',
+    'jobtitle':      '',
+    'addr_1':        'addr-1',
+    'addr_2':        'addr-2',
+    'city':          'Ostend',
+    'state':         'West Vlaanderen',
+    'postcode':      '8400',
+    'inst_address':  'Wandelaarkaai 7',
+    'instAddr2':     'Pakhuis 61',
+    'instCity':      'Oostende',
+    'instState':     '',
+    'instPostCode':  '8400',
+    'use_inst_addr': '0',
+    'deceased':      '0',
+    'retired':       '0',
+    'country':       'Belgium',
+    'relevance':     '1',
+    'instType':      '',
+    'eventtype':     '',
+    'inst_log':      ''
+  }
+}
