65c8394b44181cc5246f0db3dc15801ce3a7096f
Oleg Tsvetkov <olecve@gmail.com>
2019-07-14 19:00:37 +0300

    Provide role from UserForm to smalldata client

--- admin-app/src/components/pages/UserFormPage.js
+++ admin-app/src/components/pages/UserFormPage.js
@@ -49,8 +49,9 @@ export default function UserFormPage() {
 
   async function handleAddUserButtonClick() {
     const response = await createUser({
+      datasetIds: selectedDatasets,
       email,
-      datasetIds: selectedDatasets
+      role
     })
     if (response.exception) {
       setErrorVisible(true)
