a3171c68190bf8f50caf16e12e69c33aa029a254
Oleg Tsvetkov <olecve@gmail.com>
2019-08-22 23:27:36 +0300

    Render table with datasets if metadataProviders is empty

--- admin-app/src/components/pages/ManageDatasetPage.spec.js
+++ admin-app/src/components/pages/ManageDatasetPage.spec.js
@@ -7,31 +7,7 @@ import React from 'react'
 import { act } from 'react-dom/test-utils'
 
 jest.mock('@smalldata/dwca-lib/src/clients/SmalldataClient', () => ({
-  getDatasets: jest.fn().mockImplementation(() =>
-    new Promise((resolve) => {
-      resolve([{
-        id:                'id-1',
-        license:           {
-          title: 'title-1'
-        },
-        metadataProviders: [{
-          organizationName: 'Flanders Marine Institute (VLIZ)'
-        }],
-        title:             {
-          value: 'with organization name in metadata'
-        }
-      }, {
-        id:                'id-2',
-        license:           {
-          title: 'title-2'
-        },
-        metadataProviders: [{}],
-        title:             {
-          value: 'without organization name in metadata'
-        }
-      }])
-    })
-  )
+  getDatasets: jest.fn()
 }))
 
 describe('ManageDatasetPage', () => {
@@ -40,6 +16,32 @@ describe('ManageDatasetPage', () => {
   let wrapper
 
   it('renders correctly for non empty datasets', async() => {
+    SmalldataClient.getDatasets.mockImplementation(() =>
+      new Promise((resolve) => {
+        resolve([{
+          id:                'id-1',
+          license:           {
+            title: 'title-1'
+          },
+          metadataProviders: [{
+            organizationName: 'Flanders Marine Institute (VLIZ)'
+          }],
+          title:             {
+            value: 'with organization name in metadata'
+          }
+        }, {
+          id:                'id-2',
+          license:           {
+            title: 'title-2'
+          },
+          metadataProviders: [{}],
+          title:             {
+            value: 'without organization name in metadata'
+          }
+        }])
+      })
+    )
+
     act(() => {
       wrapper = mount(
         <MemoryRouter initialEntries={[{ pathname: '/manage-dataset', key: 'testKey' }]}>
@@ -53,5 +55,36 @@ describe('ManageDatasetPage', () => {
     wrapper.update()
     expect(wrapper).toMatchSnapshot()
     expect(wrapper.find('tbody tr')).toHaveLength(2)
+  })
+
+  it('renders correctly if metadata provider is empty', async() => {
+    SmalldataClient.getDatasets.mockImplementation(() =>
+      new Promise((resolve) => {
+        resolve([{
+          id:                'id-1',
+          license:           {
+            title: 'title-1'
+          },
+          metadataProviders: [],
+          title:             {
+            value: 'with organization name in metadata'
+          }
+        }])
+      })
+    )
+
+    act(() => {
+      wrapper = mount(
+        <MemoryRouter initialEntries={[{ pathname: '/manage-dataset', key: 'testKey' }]}>
+          <ManageDatasetPage/>
+        </MemoryRouter>
+      )
+    })
+    expect(SmalldataClient.getDatasets).toHaveBeenCalledWith()
+
+    await flushPromises()
+    wrapper.update()
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find('tbody tr')).toHaveLength(1)
   })
 })
