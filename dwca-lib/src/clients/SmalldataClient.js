bbd0718533b1b953408b289840671c4243d1ba57
Oleg Tsvetkov <olecve@gmail.com>
2019-08-24 00:45:31 +0300

    Add updateDataset function

--- dwca-lib/src/clients/SmalldataClient.js
+++ dwca-lib/src/clients/SmalldataClient.js
@@ -1,4 +1,4 @@
-import { mapUiDatasetToRequest, mapOccurrenceToDwca } from './SmalldataConverters'
+import { mapOccurrenceToDwca, mapUiDatasetToRequest } from './SmalldataConverters'
 
 const authorizationValue = 'Basic verysecret'
 const headers = {
@@ -16,6 +16,16 @@ export async function createDataset(dataset) {
   const request = mapUiDatasetToRequest(dataset)
   return await fetch('/api/datasets', {
     method: 'POST',
+    headers,
+    body:   JSON.stringify(request)
+  }).then(response => response.json())
+}
+
+export async function updateDataset(dataset, datasetId) {
+  const request = mapUiDatasetToRequest(dataset)
+  const url = `/api/datasets/${datasetId}`
+  return await fetch(url, {
+    method: 'PUT',
     headers,
     body:   JSON.stringify(request)
   }).then(response => response.json())
--- dwca-lib/src/clients/SmalldataClient.spec.js
+++ dwca-lib/src/clients/SmalldataClient.spec.js
@@ -66,6 +66,42 @@ describe('SmalldataClient', () => {
     })
   })
 
+  describe('updateDataset()', () => {
+    beforeEach(() => {
+      global.fetch = jest.fn().mockImplementation(() =>
+        new Promise((resolve) => {
+          resolve({ json: () => 'default-response' })
+        })
+      )
+    })
+
+    afterEach(() => {
+      global.fetch.mockRestore()
+    })
+
+    it('makes default request', async() => {
+      await SmalldataClient.updateDataset({
+        basicInformation:  {
+          title:        'title',
+          languageCode: 'en',
+          licence:      {
+            url:   'licence url',
+            title: 'licence title'
+          },
+          abstract:     'abstract'
+        },
+        resourceContacts:  [],
+        resourceCreators:  [],
+        metadataProviders: [],
+        keywords:          ['keyword-1']
+      }, 'some-datasetId')
+      expect(fetch.mock.calls[0][0]).toBe('/api/datasets/some-datasetId')
+      expect(fetch.mock.calls[0][1].method).toBe('PUT')
+      expect(fetch.mock.calls[0][1].headers).toEqual(expectedHeaders)
+      expect(JSON.parse(fetch.mock.calls[0][1].body)).toMatchSnapshot()
+    })
+  })
+
   describe('getOccurrences()', () => {
     beforeEach(() => {
       global.fetch = jest.fn().mockImplementation(() =>
