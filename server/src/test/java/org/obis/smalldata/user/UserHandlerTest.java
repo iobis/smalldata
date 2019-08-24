cf9b0e16624a0a27ace1ee39e9c2c332f0da78d1
Oleg Tsvetkov <olecve@gmail.com>
2019-08-24 20:36:22 +0300

    Add insertUserWithNameAndRole test

--- server/src/test/java/org/obis/smalldata/user/UserHandlerTest.java
+++ server/src/test/java/org/obis/smalldata/user/UserHandlerTest.java
@@ -27,6 +27,8 @@ public class UserHandlerTest {
   private static final String KEY_BULKINESS = "bulkiness";
   private static final String KEY_DATASET_REFS = "dataset_refs";
   private static final String KEY_EMAIL_ADDRESS = "emailAddress";
+  private static final String KEY_ROLE = "role";
+  private static final String KEY_NAME = "name";
   private static final String KEY_REF = "_ref";
   private static final String KEY_VALUE = "value";
   private static final String QUERY_REF = "ref";
@@ -126,6 +128,35 @@ public class UserHandlerTest {
           assertThat(body.getMap())
             .containsOnlyKeys(KEY_REF, KEY_EMAIL_ADDRESS, KEY_BULKINESS)
             .containsEntry(KEY_EMAIL_ADDRESS, DEFAULT_EMAIL_ADDRESS);
+          assertThat(body.getJsonObject(KEY_BULKINESS).getDouble(KEY_VALUE))
+            .isEqualTo(0.0);
+          testContext.completeNow();
+        } else {
+          testContext.failNow(ar.cause());
+        }
+      });
+  }
+
+  @Test
+  @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
+  void insertUserWithNameAndRole(Vertx vertx, VertxTestContext testContext) {
+    vertx.eventBus().<JsonObject>send(
+      Collections.USERS.dbName(),
+      new JsonObject(Map.of(
+        KEY_ACTION, "insert",
+        "user", new JsonObject()
+          .put(KEY_EMAIL_ADDRESS, DEFAULT_EMAIL_ADDRESS)
+          .put(KEY_NAME, "Indiana Jones")
+          .put(KEY_ROLE, "node manager"))),
+      ar -> {
+        if (ar.succeeded()) {
+          assertThat(ar.succeeded()).isTrue();
+          var body = ar.result().body();
+          assertThat(body.getMap())
+            .containsOnlyKeys(KEY_REF, KEY_EMAIL_ADDRESS, KEY_BULKINESS, KEY_NAME, KEY_ROLE)
+            .containsEntry(KEY_EMAIL_ADDRESS, DEFAULT_EMAIL_ADDRESS)
+            .containsEntry(KEY_NAME, "Indiana Jones")
+            .containsEntry(KEY_ROLE, "node manager");
           assertThat(body.getJsonObject(KEY_BULKINESS).getDouble(KEY_VALUE))
             .isEqualTo(0.0);
           testContext.completeNow();
