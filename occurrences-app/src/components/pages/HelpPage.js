34f7efe6e3e1b3bf6d41989a44bdde8f3c8625ea
Oleg Tsvetkov <olecve@gmail.com>
2019-08-27 17:01:02 +0300

    Rename helppage class -> help-page and update HelpPage formatting

--- occurrences-app/src/components/pages/HelpPage.js
+++ occurrences-app/src/components/pages/HelpPage.js
@@ -4,6 +4,13 @@ import { useTranslation } from 'react-i18next'
 export default function HelpPage() {
   const { t } = useTranslation()
 
-  return <div className="helppage"><h3 className="title is-3">  Entering large datasets</h3><p>In case of large datasets being inserted, please <a href={t('helpPage.contactEmail')}>contact
-    your node manager</a> for a more efficient method. In case of technical difficulties, please do the same</p></div>
+  return (
+    <div className="help-page">
+      <h3 className="title is-3">Entering large datasets</h3>
+      <p>
+        In case of large datasets being inserted, please <a href={t('helpPage.contactEmail')}>contact your node
+        manager</a> for a more efficient method. In case of technical difficulties, please do the same
+      </p>
+    </div>
+  )
 }
