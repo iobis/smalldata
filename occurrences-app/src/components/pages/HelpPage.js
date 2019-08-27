2549a68cbd2f282473dd808c19e6e23cad2e5d0f
Jill VandenDriessche <jill.vandendriessche@howest.be>
2019-08-27 15:51:09 +0200

    change help to entering large datasets
    Signed-off-by: Jill VandenDriessche <jill.vandendriessche@howest.be>

--- occurrences-app/src/components/pages/HelpPage.js
+++ occurrences-app/src/components/pages/HelpPage.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 export default function HelpPage() {
-  return <div><h3 className="title is-3">Help Page</h3></div>
+  const { t } = useTranslation()
+
+  return <div className="helppage"><h3 className="title is-3">  Entering large datasets</h3><p>In case of large datasets being inserted, please <a href={t('helpPage.contactEmail')}>contact
+    your node manager</a> for a more efficient method. In case of technical difficulties, please do the same</p></div>
 }
