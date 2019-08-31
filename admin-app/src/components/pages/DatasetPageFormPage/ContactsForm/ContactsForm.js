c19e501762498320f317b2a85873a53a96e266ca
Oleg Tsvetkov <olecve@gmail.com>
2019-08-31 12:59:40 +0300

    Update js code formatting with eslint to be consistent

--- admin-app/src/components/pages/DatasetPageFormPage/ContactsForm/ContactsForm.js
+++ admin-app/src/components/pages/DatasetPageFormPage/ContactsForm/ContactsForm.js
@@ -51,7 +51,6 @@ export default function ContactsForm({ className, contactsTableHeader, data, onC
         </div>
       </div>
 
-
       <div className="column field">
         <div className="title is-5 contacts-table-header">{contactsTableHeader}</div>
         <table className="general table is-fullwidth is-striped is-hoverable">
