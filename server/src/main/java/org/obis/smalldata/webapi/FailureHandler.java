3834a3be9ccffcbb88979adb39b54a836b61767a
Oleg Tsvetkov <olecve@gmail.com>
2019-09-01 11:16:29 +0300

    Provide error response as json with some minimalistic information

--- server/src/main/java/org/obis/smalldata/webapi/FailureHandler.java
+++ server/src/main/java/org/obis/smalldata/webapi/FailureHandler.java
@@ -7,23 +7,30 @@ import io.vertx.ext.web.RoutingContext;
 import static org.pmw.tinylog.Logger.info;
 
 class FailureHandler {
-
   public static void fallback(RoutingContext context) {
-    info(context.failure());
-    info(context.response());
-    if (null == context.failure()) {
+    if (context.failure() == null) {
+      info("Fallback with response {}", context.response().getStatusCode());
       if (context.statusCode() > 0) {
+        var error = new JsonObject()
+          .put("exception", "not specified")
+          .put("statusCode", context.statusCode());
         context
           .response()
           .setStatusCode(context.statusCode())
-          .end();
+          .putHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8")
+          .end(error.encode());
       } else {
+        var error = new JsonObject()
+          .put("exception", "not specified")
+          .put("statusCode", 500);
         context
           .response()
           .setStatusCode(500)
-          .end();
+          .putHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8")
+          .end(error.encode());
       }
     } else {
+      info(context.failure(), "Fallback to failure with response {}", context.response());
       var error = new JsonObject()
         .put("timestamp", System.nanoTime())
         .put("exception", context.failure().getClass().getName())
