6cae324be9e859f476b8931a1d8a8581055db2d6
Oleg Tsvetkov <olecve@gmail.com>
2019-09-07 11:29:04 +0300

    Create simple AccessRestrictedPage

---
+++ admin-app/src/components/pages/AccessRestrictedPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AccessRestrictedPage() {
+  return (
+    <div className="has-text-centered">
+      <p>Page Access Restricted.</p>
+      <p>Please contact node manager for additional information</p>
+    </div>
+  )
+}
