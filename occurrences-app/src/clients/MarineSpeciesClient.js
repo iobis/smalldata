e769d7656a1d9c811b5bd565a4722ce49dc497aa
Pieter Provoost <pieterprovoost@gmail.com>
2021-09-12 13:19:36 +0200

    changed worms to https endpoint

--- occurrences-app/src/clients/MarineSpeciesClient.js
+++ occurrences-app/src/clients/MarineSpeciesClient.js
@@ -1,7 +1,7 @@
 export function getByName(scientificName) {
   const defaultResult = []
   if (!scientificName || !scientificName.trim()) return defaultResult
-  const url = 'http://www.marinespecies.org/rest/AphiaRecordsByName/' + scientificName.trim() + '?like=true&marine_only=false'
+  const url = 'https://www.marinespecies.org/rest/AphiaRecordsByName/' + scientificName.trim() + '?like=true&marine_only=false'
   return fetch(url)
     .then((res) => res.text())
     .then((text) => text.length ? JSON.parse(text).filter(isRecordAvailable) : defaultResult)
@@ -15,7 +15,7 @@ export function getById(scientificNameId) {
   const defaultResult = {}
   if (!scientificNameId) return defaultResult
   const id = scientificNameId.split(':').pop()
-  const url = 'http://www.marinespecies.org/rest/AphiaRecordByAphiaID/' + id
+  const url = 'https://www.marinespecies.org/rest/AphiaRecordByAphiaID/' + id
   return fetch(url)
     .then((res) => res.text())
     .then((text) => text.length ? JSON.parse(text) : defaultResult)
